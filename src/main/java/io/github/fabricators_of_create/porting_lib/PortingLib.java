74be409edd6fc1955098399485ae266bb191121f
TropheusJ <jverrellijr@icloud.com>
2022-02-25 22:01:48 -0500

    renames and licensing and stuff

--- src/main/java/io/github/fabricators_of_create/porting_lib/PortingLib.java
+++ src/main/java/io/github/fabricators_of_create/porting_lib/PortingLib.java
@@ -1,4 +1,4 @@
-package io.github.tropheusj.modid;
+package io.github.fabricators_of_create.porting_lib;
 
 import net.fabricmc.api.ModInitializer;
 
@@ -7,8 +7,8 @@ import net.minecraft.resources.ResourceLocation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
-	public static final String ID = "modid";
+public class PortingLib implements ModInitializer {
+	public static final String ID = "porting_lib";
 	public static final Logger LOGGER = LoggerFactory.getLogger(ID);
 
 	@Override
