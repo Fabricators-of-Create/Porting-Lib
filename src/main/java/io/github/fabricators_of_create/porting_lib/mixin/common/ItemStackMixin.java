182f626edaf604f232f876ffe51bb94bd29ba683
alpha <alphamodeyt@gmail.com>
2022-03-26 12:50:38 -0500

    fix build?

--- src/main/java/io/github/fabricators_of_create/porting_lib/mixin/common/ItemStackMixin.java
+++ src/main/java/io/github/fabricators_of_create/porting_lib/mixin/common/ItemStackMixin.java
@@ -1,20 +1,5 @@
 package io.github.fabricators_of_create.porting_lib.mixin.common;
 
-import com.google.common.collect.Multimap;
-
-import io.github.fabricators_of_create.porting_lib.extensions.ItemStackExtensions;
-
-import io.github.fabricators_of_create.porting_lib.item.ToolActionCheckingItem;
-import io.github.fabricators_of_create.porting_lib.util.AttributeModiferItem;
-import io.github.fabricators_of_create.porting_lib.util.CorrectToolItem;
-import io.github.fabricators_of_create.porting_lib.util.DamagableItem;
-import io.github.fabricators_of_create.porting_lib.util.ToolAction;
-
-import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.entity.ai.attributes.Attribute;
-import net.minecraft.world.entity.ai.attributes.AttributeModifier;
-import net.minecraft.world.level.block.state.BlockState;
-
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -24,13 +9,23 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import io.github.fabricators_of_create.porting_lib.item.CustomMaxCountItem;
-import io.github.fabricators_of_create.porting_lib.util.MixinHelper;
-import io.github.fabricators_of_create.porting_lib.util.NBTSerializable;
+import com.google.common.collect.Multimap;
 
+import io.github.fabricators_of_create.porting_lib.extensions.ItemStackExtensions;
+import io.github.fabricators_of_create.porting_lib.item.CustomMaxCountItem;
+import io.github.fabricators_of_create.porting_lib.item.ToolActionCheckingItem;
+import io.github.fabricators_of_create.porting_lib.util.AttributeModiferItem;
+import io.github.fabricators_of_create.porting_lib.util.CorrectToolItem;
+import io.github.fabricators_of_create.porting_lib.util.DamagableItem;
+import io.github.fabricators_of_create.porting_lib.util.NBTSerializable;
+import io.github.fabricators_of_create.porting_lib.util.ToolAction;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.state.BlockState;
 
 @Mixin(ItemStack.class)
 public abstract class ItemStackMixin implements NBTSerializable, ItemStackExtensions {
