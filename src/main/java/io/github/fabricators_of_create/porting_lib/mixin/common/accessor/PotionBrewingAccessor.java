1ec45c5721badfcdc4b4eebef9e8ddae5af36ca4
TropheusJ <jverrellijr@icloud.com>
2022-04-03 16:28:13 -0400

    assorted accessors

--- src/main/java/io/github/fabricators_of_create/porting_lib/mixin/common/accessor/PotionBrewingAccessor.java
+++ src/main/java/io/github/fabricators_of_create/porting_lib/mixin/common/accessor/PotionBrewingAccessor.java
@@ -1,6 +1,12 @@
 package io.github.fabricators_of_create.porting_lib.mixin.common.accessor;
 
 import java.util.List;
+import java.util.function.Predicate;
+
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+
+import net.minecraft.world.item.alchemy.Potion;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
@@ -10,8 +16,23 @@ import net.minecraft.world.item.crafting.Ingredient;
 
 @Mixin(PotionBrewing.class)
 public interface PotionBrewingAccessor {
-	@Accessor
-	static List<Ingredient> getALLOWED_CONTAINERS() {
-		throw new UnsupportedOperationException();
+	@Accessor("CONTAINER_MIXES")
+	static List<PotionBrewing.Mix<Item>> port_lib$CONTAINER_MIXES() {
+		throw new RuntimeException("mixin failed!");
+	}
+
+	@Accessor("POTION_MIXES")
+	static List<PotionBrewing.Mix<Potion>> port_lib$POTION_MIXES() {
+		throw new RuntimeException("mixin failed!");
+	}
+
+	@Accessor("ALLOWED_CONTAINERS")
+	static List<Ingredient> port_lib$ALLOWED_CONTAINERS() {
+		throw new RuntimeException("mixin failed!");
+	}
+
+	@Accessor("ALLOWED_CONTAINER")
+	static Predicate<ItemStack> port_lib$ALLOWED_CONTAINER() {
+		throw new RuntimeException("mixin failed!");
 	}
 }
--- src/main/java/io/github/fabricators_of_create/porting_lib/mixin/common/accessor/RecipeManagerAccessor.java
+++ src/main/java/io/github/fabricators_of_create/porting_lib/mixin/common/accessor/RecipeManagerAccessor.java
@@ -8,12 +8,16 @@ import net.minecraft.world.item.crafting.RecipeManager;
 import net.minecraft.world.item.crafting.RecipeType;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
 import java.util.Map;
 
 @Mixin(RecipeManager.class)
 public interface RecipeManagerAccessor {
+	@Accessor("recipes")
+	Map<RecipeType<?>, Map<ResourceLocation, Recipe<?>>> port_lib$getRecipes();
+
 	@Invoker("byType")
 	<C extends Container, T extends Recipe<C>> Map<ResourceLocation, Recipe<C>> port_lib$byType(RecipeType<T> recipeType);
 }
