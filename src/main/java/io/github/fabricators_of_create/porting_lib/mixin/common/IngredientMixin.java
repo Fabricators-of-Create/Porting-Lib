97bbd72ff44e4a4264f51f8c5562eae67d95f797
TropheusJ <jverrellijr@icloud.com>
2022-04-06 15:30:40 -0400

    such a simple fix...

--- src/main/java/io/github/fabricators_of_create/porting_lib/mixin/common/IngredientMixin.java
+++ src/main/java/io/github/fabricators_of_create/porting_lib/mixin/common/IngredientMixin.java
@@ -1,17 +1,18 @@
 package io.github.fabricators_of_create.porting_lib.mixin.common;
 
-import java.util.stream.Stream;
-
 import io.github.fabricators_of_create.porting_lib.crafting.CraftingHelper;
 import io.github.fabricators_of_create.porting_lib.crafting.IIngredientSerializer;
 import io.github.fabricators_of_create.porting_lib.crafting.VanillaIngredientSerializer;
 import io.github.fabricators_of_create.porting_lib.extensions.IngredientExtensions;
+
+import net.minecraft.world.item.crafting.Ingredient.Value;
 
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -21,7 +22,10 @@ import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Ingredient;
 
-@Mixin(Ingredient.class)
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+@Mixin(value = Ingredient.class, priority = 234) // magic random number to make sure we apply in the correct order
 public abstract class IngredientMixin implements IngredientExtensions {
 
     @Shadow
@@ -35,34 +39,44 @@ public abstract class IngredientMixin implements IngredientExtensions {
 	@Final
 	public static Ingredient EMPTY;
 
-    @Inject(method = "toNetwork", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/crafting/Ingredient;dissolve()V"), cancellable = true)
-    private void toNetwork(FriendlyByteBuf buffer, CallbackInfo ci) {
-        if (!this.isVanilla()) {
+	@Shadow
+	@Final
+	private Value[] values;
+	@Unique
+	private boolean port_lib$simple;
+	@Unique
+	private final boolean port_lib$vanilla = this.getClass().equals(Ingredient.class);
+
+	@Inject(method = "<init>", at = @At("TAIL"))
+	private void port_lib$init(Stream<? extends Ingredient.Value> stream, CallbackInfo ci) {
+		port_lib$simple = Arrays.stream(values).noneMatch(list -> list.getItems().stream().anyMatch(stack -> stack.getItem().canBeDepleted()));
+	}
+
+    @Inject(method = "toNetwork", at = @At(value = "INVOKE", shift = Shift.AFTER, target = "Lnet/minecraft/world/item/crafting/Ingredient;dissolve()V"), cancellable = true)
+    private void port_lib$toNetwork(FriendlyByteBuf buffer, CallbackInfo ci) {
+		buffer.writeBoolean(port_lib$vanilla);
+        if (!port_lib$vanilla) {
             CraftingHelper.write(buffer, (Ingredient) (Object) this);
             ci.cancel();
         }
     }
 
     @Inject(method = "fromNetwork", at = @At("HEAD"), cancellable = true)
-    private static void fromNetwork(FriendlyByteBuf buffer, CallbackInfoReturnable<Ingredient> cir) {
-        int size = buffer.readVarInt();
-        if (size == -1)
+    private static void port_lib$fromNetwork(FriendlyByteBuf buffer, CallbackInfoReturnable<Ingredient> cir) {
+		boolean vanilla = buffer.readBoolean();
+        if (!vanilla)
 			cir.setReturnValue(CraftingHelper.getIngredient(buffer.readResourceLocation(), buffer));
     }
 
     @Override
     public IIngredientSerializer<? extends Ingredient> getSerializer() {
-        if (!isVanilla()) throw new IllegalStateException("Modders must implement Ingredient.getSerializer in their custom Ingredients: " + this);
+        if (!port_lib$vanilla) throw new IllegalStateException("Modders must implement Ingredient.getSerializer in their custom Ingredients: " + this);
             return VanillaIngredientSerializer.INSTANCE;
-    }
-
-    public final boolean isVanilla() {
-        return this.getClass().equals(Ingredient.class);
     }
 
     @Override
     public boolean isSimple() {
-        return (Object) this == EMPTY;
+        return (Object) this == EMPTY || port_lib$simple;
     }
 
     @Override
