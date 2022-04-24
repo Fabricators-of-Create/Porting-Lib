2bdb9e8977ac9f572e2fe93799a862b55743b202
alpha <alphamodeyt@gmail.com>
2022-04-24 09:48:49 -0500

    add pack finder event

---
+++ src/main/java/io/github/fabricators_of_create/porting_lib/event/common/AddPackFindersCallback.java
@@ -0,0 +1,18 @@
+package io.github.fabricators_of_create.porting_lib.event.common;
+
+import java.util.function.Consumer;
+
+import net.fabricmc.fabric.api.event.Event;
+import net.fabricmc.fabric.api.event.EventFactory;
+import net.minecraft.server.packs.PackType;
+import net.minecraft.server.packs.repository.RepositorySource;
+
+public interface AddPackFindersCallback {
+	Event<AddPackFindersCallback> EVENT = EventFactory.createArrayBacked(AddPackFindersCallback.class, callbacks -> (packType, sources) -> {
+		for (AddPackFindersCallback e : callbacks) {
+			e.addPack(packType, sources);
+		}
+	});
+
+	void addPack(PackType packType, Consumer<RepositorySource> sources);
+}
