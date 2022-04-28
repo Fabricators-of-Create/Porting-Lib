cb3ade966d904fc03fcf2179ed55cdb43b460aed
alpha <alphamodeyt@gmail.com>
2022-04-27 19:43:15 -0500

    canRiderInteract and re add interface injection as a test again

--- src/main/java/io/github/fabricators_of_create/porting_lib/extensions/EntityExtensions.java
+++ src/main/java/io/github/fabricators_of_create/porting_lib/extensions/EntityExtensions.java
@@ -17,4 +17,14 @@ public interface EntityExtensions {
 	default Collection<ItemEntity> captureDrops(Collection<ItemEntity> value) {
 		throw new RuntimeException("this should be overridden via mixin. what?");
 	}
+
+	/**
+	 * If a rider of this entity can interact with this entity. Should return true on the
+	 * ridden entity if so.
+	 *
+	 * @return if the entity can be interacted with from a rider
+	 */
+	default boolean canRiderInteract() {
+		return false;
+	}
 }
