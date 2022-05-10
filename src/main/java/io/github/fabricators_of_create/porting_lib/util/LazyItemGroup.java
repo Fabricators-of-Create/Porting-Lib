db29e7a572737308e40389446e2d667b551788cb
TropheusJ <jverrellijr@icloud.com>
2022-05-10 06:31:20 -0400

    remove FAPI impl use

--- src/main/java/io/github/fabricators_of_create/porting_lib/util/LazyItemGroup.java
+++ src/main/java/io/github/fabricators_of_create/porting_lib/util/LazyItemGroup.java
@@ -1,6 +1,5 @@
 package io.github.fabricators_of_create.porting_lib.util;
 
-import net.fabricmc.fabric.impl.item.group.ItemGroupExtensions;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 
@@ -11,7 +10,7 @@ public class LazyItemGroup extends CreativeModeTab {
 	private final Supplier<ItemStack> stackSupplier;
 
 	public LazyItemGroup(String name, Supplier<ItemStack> stackSupplier) {
-		super(expand(), name);
+		super(ItemGroupUtil.expandArrayAndGetId(), name);
 		this.stackSupplier = stackSupplier;
 	}
 
@@ -22,10 +21,5 @@ public class LazyItemGroup extends CreativeModeTab {
 	@Override
 	public ItemStack makeIcon() {
 		return stackSupplier.get();
-	}
-
-	public static int expand() {
-		((ItemGroupExtensions)CreativeModeTab.TAB_BUILDING_BLOCKS).fabric_expandArray();
-		return CreativeModeTab.TABS.length - 1;
 	}
 }
