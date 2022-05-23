255e858cbadad121559376fcb99098a58eb2aeb2
alpha <alphamodeyt@gmail.com>
2022-05-23 17:32:26 -0500

    ModifyItemAttributeModifiersCallback exists so ItemAttributeModifierCallback is not needed anymore

--- src/main/java/io/github/fabricators_of_create/porting_lib/render/TransformTypeDependentItemBakedModel.java
+++ src/main/java/io/github/fabricators_of_create/porting_lib/render/TransformTypeDependentItemBakedModel.java
@@ -4,7 +4,6 @@ import com.mojang.blaze3d.vertex.PoseStack;
 
 import com.mojang.math.Transformation;
 
-import io.github.fabricators_of_create.porting_lib.extensions.TransformationExtensions;
 import io.github.fabricators_of_create.porting_lib.util.TransformationHelper;
 import net.minecraft.client.renderer.block.model.ItemTransforms.TransformType;
 import net.minecraft.client.resources.model.BakedModel;
@@ -12,8 +11,8 @@ import net.minecraft.client.resources.model.BakedModel;
 public interface TransformTypeDependentItemBakedModel {
 	default BakedModel handlePerspective(TransformType type, PoseStack stack) {
 		Transformation tr = TransformationHelper.toTransformation(((BakedModel) this).getTransforms().getTransform(type));
-		if(!((TransformationExtensions)(Object)tr).isIdentity()) {
-			((TransformationExtensions)(Object)tr).push(stack);
+		if(!tr.isIdentity()) {
+			tr.push(stack);
 		}
 		return (BakedModel) this;
 	}
