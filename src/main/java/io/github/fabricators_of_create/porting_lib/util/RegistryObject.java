7337e1fbcfcd994d8adee1ef1e7909ae0684418d
alpha <alphamodeyt@gmail.com>
2022-05-25 16:27:58 -0500

    RegistryObject holders

--- src/main/java/io/github/fabricators_of_create/porting_lib/util/RegistryObject.java
+++ src/main/java/io/github/fabricators_of_create/porting_lib/util/RegistryObject.java
@@ -1,12 +1,17 @@
 package io.github.fabricators_of_create.porting_lib.util;
 
+import java.util.Optional;
 import java.util.function.Supplier;
 
 import com.google.common.base.Suppliers;
 
+import net.minecraft.core.Holder;
+import net.minecraft.core.Registry;
+import net.minecraft.data.BuiltinRegistries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 @SuppressWarnings("CanBeRecord")
@@ -16,6 +21,9 @@ public final class RegistryObject<T> implements Supplier<T> {
 	private Supplier<T> wrappedEntry;
 	@Nullable
 	private final ResourceKey<T> key;
+
+	@Nullable
+	private Holder<T> holder;
 
 	public RegistryObject(ResourceLocation id, Supplier<T> wrappedEntry, ResourceKey<?> key) {
 		this.id = id;
@@ -39,5 +47,26 @@ public final class RegistryObject<T> implements Supplier<T> {
 	@Nullable
 	public ResourceKey<T> getKey() {
 		return this.key;
+	}
+
+	@SuppressWarnings("unchecked")
+	@NotNull
+	public Optional<Holder<T>> getHolder() {
+		if (this.holder == null && this.key != null && registryExists(this.key.registry())) {
+			ResourceLocation registryName = this.key.registry();
+			Registry<T> registry = (Registry<T>) Registry.REGISTRY.get(registryName);
+			if (registry == null)
+				registry = (Registry<T>) BuiltinRegistries.REGISTRY.get(registryName);
+
+			if (registry != null)
+				this.holder = registry.getOrCreateHolder(this.key);
+		}
+
+		return Optional.ofNullable(this.holder);
+	}
+
+	private static boolean registryExists(ResourceLocation registryName) {
+		return Registry.REGISTRY.containsKey(registryName)
+				|| BuiltinRegistries.REGISTRY.containsKey(registryName);
 	}
 }
