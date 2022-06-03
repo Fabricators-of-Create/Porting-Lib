6c764fb8fc4db7f64ebba6ec56b16e67f72afb15
TropheusJ <jverrellijr@icloud.com>
2022-06-03 04:39:11 -0400

    fix crash in prod

--- src/main/java/io/github/fabricators_of_create/porting_lib/mixin/common/EnchantmentMenuMixin.java
+++ src/main/java/io/github/fabricators_of_create/porting_lib/mixin/common/EnchantmentMenuMixin.java
@@ -2,6 +2,7 @@ package io.github.fabricators_of_create.porting_lib.mixin.common;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Group;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 import io.github.fabricators_of_create.porting_lib.enchant.EnchantmentBonusBlock;
@@ -13,9 +14,26 @@ import net.minecraft.world.level.block.EnchantmentTableBlock;
 
 @Mixin(EnchantmentMenu.class)
 public class EnchantmentMenuMixin {
-	@ModifyVariable(method = "m_mpsetdhw", at = @At(value = "STORE", ordinal = 0), ordinal = 0)
-	private int port_lib$modifyEnchantValue(int obj, ItemStack stack, Level level, BlockPos pos) {
-		for(BlockPos blockPos : EnchantmentTableBlock.BOOKSHELF_OFFSETS) {
+	@Group(name = "enchantValue", min = 1, max = 1)
+	@ModifyVariable(method = "m_mpsetdhw", at = @At(value = "STORE", ordinal = 0), ordinal = 0, require = 0)
+	private int port_lib$modifyEnchantValueHashed(int obj, ItemStack stack, Level level, BlockPos pos) {
+		return port_lib$modifyEnchantValueImpl(obj, stack, level, pos);
+	}
+
+	@Group(name = "enchantValue", min = 1, max = 1)
+	@ModifyVariable(method = "method_17411", at = @At(value = "STORE", ordinal = 0), ordinal = 0, require = 0)
+	private int port_lib$modifyEnchantValueIntermediary(int obj, ItemStack stack, Level level, BlockPos pos) {
+		return port_lib$modifyEnchantValueImpl(obj, stack, level, pos);
+	}
+
+	@Group(name = "enchantValue", min = 1, max = 1)
+	@ModifyVariable(method = "lambda$slotsChanged$0", at = @At(value = "STORE", ordinal = 0), ordinal = 0, require = 0)
+	private int port_lib$modifyEnchantValueMojang(int obj, ItemStack stack, Level level, BlockPos pos) {
+		return port_lib$modifyEnchantValueImpl(obj, stack, level, pos);
+	}
+
+	private int port_lib$modifyEnchantValueImpl(int obj, ItemStack stack, Level level, BlockPos pos) {
+		for (BlockPos blockPos : EnchantmentTableBlock.BOOKSHELF_OFFSETS) {
 			if (level.getBlockState(pos.offset(blockPos)).getBlock() instanceof EnchantmentBonusBlock bonusBlock)
 				obj += bonusBlock.getEnchantPowerBonus(level.getBlockState(pos.offset(blockPos)), level, pos.offset(blockPos));
 		}
