99fe759b6403514c6269d5eeda7a1ae2406108f8
TropheusJ <jverrellijr@icloud.com>
2022-06-04 05:14:02 -0400

    maybe?

--- src/main/java/io/github/fabricators_of_create/porting_lib/mixin/common/EnchantmentMenuMixin.java
+++ src/main/java/io/github/fabricators_of_create/porting_lib/mixin/common/EnchantmentMenuMixin.java
@@ -1,6 +1,7 @@
 package io.github.fabricators_of_create.porting_lib.mixin.common;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Group;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
@@ -26,12 +27,7 @@ public class EnchantmentMenuMixin {
 		return port_lib$modifyEnchantValueImpl(obj, stack, level, pos);
 	}
 
-	@Group(name = "enchantValue", min = 1, max = 2)
-	@ModifyVariable(method = "lambda$slotsChanged$0", at = @At(value = "STORE", ordinal = 0), ordinal = 0, require = 0, remap = false)
-	private int port_lib$modifyEnchantValueMojang(int obj, ItemStack stack, Level level, BlockPos pos) {
-		return port_lib$modifyEnchantValueImpl(obj, stack, level, pos);
-	}
-
+	@Unique
 	private int port_lib$modifyEnchantValueImpl(int obj, ItemStack stack, Level level, BlockPos pos) {
 		for (BlockPos blockPos : EnchantmentTableBlock.BOOKSHELF_OFFSETS) {
 			if (level.getBlockState(pos.offset(blockPos)).getBlock() instanceof EnchantmentBonusBlock bonusBlock)
