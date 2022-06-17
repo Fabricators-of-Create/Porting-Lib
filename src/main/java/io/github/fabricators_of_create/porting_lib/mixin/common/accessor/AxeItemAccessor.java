ead8e37f08fb9c9fbc90fd7ea030d98e48cb3d2e
TropheusJ <jverrellijr@icloud.com>
2022-06-16 22:02:31 -0400

    stripped away

--- src/main/java/io/github/fabricators_of_create/porting_lib/mixin/common/accessor/AxeItemAccessor.java
+++ src/main/java/io/github/fabricators_of_create/porting_lib/mixin/common/accessor/AxeItemAccessor.java
@@ -1,24 +1,16 @@
 package io.github.fabricators_of_create.porting_lib.mixin.common.accessor;
 
 import net.minecraft.world.item.AxeItem;
-import net.minecraft.world.level.block.Block;
 
 import net.minecraft.world.level.block.state.BlockState;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
-import java.util.Map;
 import java.util.Optional;
 
 @Mixin(AxeItem.class)
 public interface AxeItemAccessor {
-	@Accessor("STRIPPABLES")
-	static Map<Block, Block> porting_lib$STRIPPABLES() {
-		throw new RuntimeException("mixin failed");
-	}
-
 	@Invoker("getStripped")
 	Optional<BlockState> porting_lib$getStripped(BlockState unstrippedState);
 }
