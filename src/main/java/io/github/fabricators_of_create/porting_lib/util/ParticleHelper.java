1e8234b00f7c228543e1939cbe3c84e350a997c9
TropheusJ <jverrellijr@icloud.com>
2022-06-16 22:36:54 -0400

    changing sprites

--- src/main/java/io/github/fabricators_of_create/porting_lib/util/ParticleHelper.java
+++ src/main/java/io/github/fabricators_of_create/porting_lib/util/ParticleHelper.java
@@ -2,12 +2,29 @@ package io.github.fabricators_of_create.porting_lib.util;
 
 import io.github.fabricators_of_create.porting_lib.mixin.client.accessor.ParticleAccessor;
 
+import io.github.fabricators_of_create.porting_lib.mixin.client.accessor.TextureSheetParticleAccessor;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.particle.Particle;
+import net.minecraft.client.particle.TextureSheetParticle;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.world.level.block.state.BlockState;
 
+@Environment(EnvType.CLIENT)
 public final class ParticleHelper {
-
 	public static void setStoppedByCollision(Particle particle, boolean bool) {
 		((ParticleAccessor) particle).port_lib$stoppedByCollision(bool);
+	}
+
+	public static void setSprite(TextureSheetParticle particle, TextureAtlasSprite sprite) {
+		((TextureSheetParticleAccessor) particle).porting_lib$setSprite(sprite);
+	}
+
+	public static void updateSprite(TextureSheetParticle particle, BlockState state) {
+		TextureAtlasSprite sprite = Minecraft.getInstance().getBlockRenderer()
+				.getBlockModelShaper().getParticleIcon(state);
+		setSprite(particle, sprite);
 	}
 
 	private ParticleHelper() {}
