0506e1a5c749fef36cc3417953db1962d1bcc6eb
alpha <alphamodeyt@gmail.com>
2022-07-31 14:25:23 -0500

    JsonCodecProvider, Deprecate KeyBindingHelper#getKeyCode, Update MaterialChest to use  generics

--- src/main/java/io/github/fabricators_of_create/porting_lib/util/KeyBindingHelper.java
+++ src/main/java/io/github/fabricators_of_create/porting_lib/util/KeyBindingHelper.java
@@ -15,6 +15,10 @@ public final class KeyBindingHelper {
 		return keyCode != InputConstants.UNKNOWN && keyCode.equals(getKeyCode(keyMapping));
 	}
 
+	/**
+	 * Use {@link net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper#getBoundKeyOf(KeyMapping)} instead
+	 */
+	@Deprecated(forRemoval = true)
 	public static InputConstants.Key getKeyCode(KeyMapping keyBinding) {
 		return get(keyBinding).port_lib$getKey();
 	}
--- src/main/java/io/github/fabricators_of_create/porting_lib/util/MaterialChest.java
+++ src/main/java/io/github/fabricators_of_create/porting_lib/util/MaterialChest.java
@@ -2,8 +2,9 @@ package io.github.fabricators_of_create.porting_lib.util;
 
 import net.minecraft.client.resources.model.Material;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.LidBlockEntity;
 import net.minecraft.world.level.block.state.properties.ChestType;
 
 public interface MaterialChest {
-	Material getMaterial(BlockEntity blockEntity, ChestType chestType);
+	<T extends BlockEntity & LidBlockEntity> Material getMaterial(T blockEntity, ChestType chestType);
 }
