8240329ab8d0c8d194d355731044cd3c5dae78b0
alpha <alphamodeyt@gmail.com>
2022-08-02 21:21:22 -0500

    Backport blockstate stuff

---
+++ src/main/java/io/github/fabricators_of_create/porting_lib/mixin/common/BlockStateMixin.java
@@ -0,0 +1,11 @@
+package io.github.fabricators_of_create.porting_lib.mixin.common;
+
+import io.github.fabricators_of_create.porting_lib.extensions.BlockStateExtensions;
+
+import net.minecraft.world.level.block.state.BlockState;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(BlockState.class)
+public class BlockStateMixin implements BlockStateExtensions {
+}
