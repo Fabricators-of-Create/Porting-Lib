964484733a245a02616add1c9e684e0192a34c7c
alpha <alphamodeyt@gmail.com>
2022-09-16 11:17:09 -0500

    add default to CreativeModeTabPresentEnchantment

--- base/src/main/java/io/github/fabricators_of_create/porting_lib/enchant/CreativeModeTabPresentEnchantment.java
+++ base/src/main/java/io/github/fabricators_of_create/porting_lib/enchant/CreativeModeTabPresentEnchantment.java
@@ -2,6 +2,7 @@ package io.github.fabricators_of_create.porting_lib.enchant;
 
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.enchantment.Enchantment;
 
 /**
  * An enchantment that can be added to CreativeModeTabs via enchanted books. This also determines if the Enchantment
@@ -9,5 +10,13 @@ import net.minecraft.world.item.Item;
  * {@link CreativeModeTab#getEnchantmentCategories() enchantment categories} must also not be empty.
  */
 public interface CreativeModeTabPresentEnchantment {
-	boolean allowedInCreativeTab(Item book, CreativeModeTab tab);
+	default boolean allowedInCreativeTab(Item book, CreativeModeTab tab) {
+		/*if (!((Enchantment) this).isAllowedOnBooks()) { TODO: implement this if ever implemented
+			return false;
+		} else */if (tab == CreativeModeTab.TAB_SEARCH) {
+			return ((Enchantment) this).category != null;
+		} else {
+			return tab.hasEnchantmentCategory(((Enchantment) this).category);
+		}
+	}
 }
