74dd8ee97347f1b66a653e62ab8ab55f6e950fc6
alpha <alphamodeyt@gmail.com>
2022-09-21 23:30:43 -0500

    Use force add tag if we are using a fabric builder

--- extensions/src/main/java/io/github/fabricators_of_create/porting_lib/extensions/TagAppenderExtensions.java
+++ extensions/src/main/java/io/github/fabricators_of_create/porting_lib/extensions/TagAppenderExtensions.java
@@ -1,5 +1,6 @@
 package io.github.fabricators_of_create.porting_lib.extensions;
 
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.data.tags.TagsProvider;
 import net.minecraft.tags.TagKey;
 
@@ -7,9 +8,14 @@ public interface TagAppenderExtensions<T> {
 	@SuppressWarnings("unchecked")
 	default TagsProvider.TagAppender<T> addTags(TagKey<T>... values) {
 		TagsProvider.TagAppender<T> builder = (TagsProvider.TagAppender<T>) this;
-		for (TagKey<T> value : values) {
-			builder.addTag(value);
-		}
+		if (builder instanceof FabricTagProvider<T>.FabricTagBuilder<T> fabricTagBuilder)
+			for (TagKey<T> value : values) {
+				fabricTagBuilder.forceAddTag(value);
+			}
+		else
+			for (TagKey<T> value : values) {
+				builder.addTag(value);
+			}
 		return builder;
 	}
 }
