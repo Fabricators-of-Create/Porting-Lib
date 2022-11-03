cf4ef0a1726ec7dfd72b0989705e1eaca07dfae2
Dragon-Seeker <timekeeperguild@gmail.com>
2022-11-03 16:34:15 -0500

    Fix Incompatibility with PinLib with a rework of MapRendererMapInstanceMixin
    - Remove Overwrite of vanilla method

--- base/src/main/java/io/github/fabricators_of_create/porting_lib/mixin/client/MapRendererMapInstanceMixin.java
+++ base/src/main/java/io/github/fabricators_of_create/porting_lib/mixin/client/MapRendererMapInstanceMixin.java
@@ -1,100 +1,70 @@
 package io.github.fabricators_of_create.porting_lib.mixin.client;
 
-import org.spongepowered.asm.mixin.Final;
+import java.util.Iterator;
+
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.math.Matrix4f;
-import com.mojang.math.Vector3f;
 
-import io.github.fabricators_of_create.porting_lib.extensions.MapDecorationExtensions;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.MapRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.RenderType;
-import net.minecraft.network.chat.Component;
-import net.minecraft.util.Mth;
 import net.minecraft.world.level.saveddata.maps.MapDecoration;
-import net.minecraft.world.level.saveddata.maps.MapItemSavedData;
 
 @Mixin(MapRenderer.MapInstance.class)
 public abstract class MapRendererMapInstanceMixin {
-	@Shadow
-	private boolean requiresUpload;
+	@Unique
+	public Iterator<?> porting_Lib$cachedIterator = null;
+	@Unique public boolean porting_Lib$iteratorIsEmpty = false;
 
-	@Shadow
-	protected abstract void updateTexture();
+	@Unique private boolean porting_Lib$updateIndexFromCache = false;
+	@Unique public int porting_Lib$cached_index = 0;
 
-	@Shadow
-	@Final
-	private RenderType renderType;
+	@Inject(method = "draw", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/saveddata/maps/MapItemSavedData;getDecorations()Ljava/lang/Iterable;", shift = At.Shift.BY, by = 3), locals = LocalCapture.CAPTURE_FAILHARD)
+	private void porting_Lib$getLoopIteratorAndIndex(PoseStack poseStack, MultiBufferSource bufferSource, boolean active, int packedLight, CallbackInfo ci, int i, int j, float f, Matrix4f matrix4f, VertexConsumer vertexConsumer, int k, Iterator<?> var11){
+		this.porting_Lib$cachedIterator = var11;
+		this.porting_Lib$cached_index = k;
+	}
 
-	@Shadow
-	private MapItemSavedData data;
+	@ModifyVariable(method = "draw", at = @At(value = "STORE"))
+	private MapDecoration porting_Lib$gatherNextValueIfCustomRender(MapDecoration value){
+		while(value.render(porting_Lib$cached_index)) {
+			this.porting_Lib$cached_index++;
+			this.porting_Lib$updateIndexFromCache = true;
 
-	/**
-	 * @author AlphaMode
-	 * @reason mixin doesn't allow continuing in loops :why:
-	 */
-	@Overwrite
-	public void draw(PoseStack p_93292_, MultiBufferSource p_93293_, boolean p_93294_, int p_93295_) {
-		if (this.requiresUpload) {
-			this.updateTexture();
-			this.requiresUpload = false;
-		}
+			if (this.porting_Lib$cachedIterator.hasNext()) {
+				value = (MapDecoration) porting_Lib$cachedIterator.next();
+			} else {
+				this.porting_Lib$iteratorIsEmpty = true;
 
-		int i = 0;
-		int j = 0;
-		float f = 0.0F;
-		Matrix4f matrix4f = p_93292_.last().pose();
-		VertexConsumer vertexconsumer = p_93293_.getBuffer(this.renderType);
-		vertexconsumer.vertex(matrix4f, 0.0F, 128.0F, -0.01F).color(255, 255, 255, 255).uv(0.0F, 1.0F).uv2(p_93295_).endVertex();
-		vertexconsumer.vertex(matrix4f, 128.0F, 128.0F, -0.01F).color(255, 255, 255, 255).uv(1.0F, 1.0F).uv2(p_93295_).endVertex();
-		vertexconsumer.vertex(matrix4f, 128.0F, 0.0F, -0.01F).color(255, 255, 255, 255).uv(1.0F, 0.0F).uv2(p_93295_).endVertex();
-		vertexconsumer.vertex(matrix4f, 0.0F, 0.0F, -0.01F).color(255, 255, 255, 255).uv(0.0F, 0.0F).uv2(p_93295_).endVertex();
-		int k = 0;
-
-		for(MapDecoration mapdecoration : this.data.getDecorations()) {
-			if (!p_93294_ || mapdecoration.renderOnFrame()) {
-				if (mapdecoration.render(k)) { k++; continue; }
-				p_93292_.pushPose();
-				p_93292_.translate(0.0F + (float)mapdecoration.getX() / 2.0F + 64.0F, 0.0F + (float)mapdecoration.getY() / 2.0F + 64.0F, (double)-0.02F);
-				p_93292_.mulPose(Vector3f.ZP.rotationDegrees((float)(mapdecoration.getRot() * 360) / 16.0F));
-				p_93292_.scale(4.0F, 4.0F, 3.0F);
-				p_93292_.translate(-0.125D, 0.125D, 0.0D);
-				byte b0 = mapdecoration.getImage();
-				float f1 = (float)(b0 % 16 + 0) / 16.0F;
-				float f2 = (float)(b0 / 16 + 0) / 16.0F;
-				float f3 = (float)(b0 % 16 + 1) / 16.0F;
-				float f4 = (float)(b0 / 16 + 1) / 16.0F;
-				Matrix4f matrix4f1 = p_93292_.last().pose();
-				float f5 = -0.001F;
-				VertexConsumer vertexconsumer1 = p_93293_.getBuffer(MapRenderer.MAP_ICONS);
-				vertexconsumer1.vertex(matrix4f1, -1.0F, 1.0F, (float)k * -0.001F).color(255, 255, 255, 255).uv(f1, f2).uv2(p_93295_).endVertex();
-				vertexconsumer1.vertex(matrix4f1, 1.0F, 1.0F, (float)k * -0.001F).color(255, 255, 255, 255).uv(f3, f2).uv2(p_93295_).endVertex();
-				vertexconsumer1.vertex(matrix4f1, 1.0F, -1.0F, (float)k * -0.001F).color(255, 255, 255, 255).uv(f3, f4).uv2(p_93295_).endVertex();
-				vertexconsumer1.vertex(matrix4f1, -1.0F, -1.0F, (float)k * -0.001F).color(255, 255, 255, 255).uv(f1, f4).uv2(p_93295_).endVertex();
-				p_93292_.popPose();
-				if (mapdecoration.getName() != null) {
-					Font font = Minecraft.getInstance().font;
-					Component component = mapdecoration.getName();
-					float f6 = (float)font.width(component);
-					float f7 = Mth.clamp(25.0F / f6, 0.0F, 6.0F / 9.0F);
-					p_93292_.pushPose();
-					p_93292_.translate(0.0F + (float)mapdecoration.getX() / 2.0F + 64.0F - f6 * f7 / 2.0F, 0.0F + (float)mapdecoration.getY() / 2.0F + 64.0F + 4.0F, -0.025F);
-					p_93292_.scale(f7, f7, 1.0F);
-					p_93292_.translate(0.0D, 0.0D, -0.1F);
-					font.drawInBatch(component, 0.0F, 0.0F, -1, false, p_93292_.last().pose(), p_93293_, false, Integer.MIN_VALUE, p_93295_);
-					p_93292_.popPose();
-				}
-
-				++k;
+				//Should this be the last value that was gathered incase someone injects between somehow?
+				return null;
 			}
 		}
 
+		return value;
+	}
+
+	@Inject(method = "draw", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/saveddata/maps/MapDecoration;renderOnFrame()Z"), cancellable = true)
+	public void porting_Lib$cancelIfIteratorIsEmpty(PoseStack poseStack, MultiBufferSource bufferSource, boolean active, int packedLight, CallbackInfo ci){
+		if(porting_Lib$iteratorIsEmpty) ci.cancel();
+	}
+
+	@ModifyVariable(method = "draw", at = @At(value = "LOAD", ordinal = 0), index = 10)
+	private int porting_Lib$updateKIfNeeded(int old_k){
+		if(porting_Lib$updateIndexFromCache){
+			this.porting_Lib$updateIndexFromCache = false;
+
+			return porting_Lib$cached_index;
+		}
+
+		return old_k;
 	}
 }
