1d5204abd0bf7c6b1c4633bb2b6a7b5d853cedd2
AlphaMode <alphamodeyt@gmail.com>
2022-11-02 20:54:22 -0500

    remap attributes from the forge namespace to porting_lib

---
+++ attributes/src/main/java/io/github/fabricators_of_create/porting_lib/attributes/mixin/AttributeMapMixin.java
@@ -0,0 +1,19 @@
+package io.github.fabricators_of_create.porting_lib.attributes.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.ai.attributes.AttributeMap;
+
+@Mixin(AttributeMap.class)
+public class AttributeMapMixin {
+	@ModifyArg(method = "load", at = @At(value = "INVOKE", target = "Lnet/minecraft/resources/ResourceLocation;tryParse(Ljava/lang/String;)Lnet/minecraft/resources/ResourceLocation;"))
+	private String port_lib$remapOldAttribute(String location) {
+		if (Registry.ATTRIBUTE.getOptional(ResourceLocation.tryParse(location)).isPresent())
+			return location;
+		return location.replace("forge", "porting_lib");
+	}
+}
