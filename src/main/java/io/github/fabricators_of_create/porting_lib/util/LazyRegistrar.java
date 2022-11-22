9b6b7352cfe61e5db033495f2e156f7f98fb2acc
AlphaMode <alphamodeyt@gmail.com>
2022-11-22 15:42:17 -0600

    backport lazy registers

--- src/main/java/io/github/fabricators_of_create/porting_lib/util/LazyRegistrar.java
+++ src/main/java/io/github/fabricators_of_create/porting_lib/util/LazyRegistrar.java
@@ -1,24 +1,30 @@
 package io.github.fabricators_of_create.porting_lib.util;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Supplier;
+
+import net.fabricmc.fabric.api.event.registry.FabricRegistryBuilder;
 import net.minecraft.core.Registry;
+import net.minecraft.data.BuiltinRegistries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Supplier;
-
 public class LazyRegistrar<T> {
 	public final String mod_id;
-	private final Registry<T> registry;
-	private final List<RegistryObject<? extends T>> entires;
+	private final ResourceKey<? extends Registry<T>> registryKey;
+	private final Map<RegistryObject<T>, Supplier<? extends T>> entries = new LinkedHashMap<>();
+	private final Set<RegistryObject<T>> entriesView = Collections.unmodifiableSet(entries.keySet());
 
-	LazyRegistrar(Registry<T> registry, String modid) {
-		this.registry = registry;
+	LazyRegistrar(ResourceKey<? extends Registry<T>> registryKey, String modid) {
+		this.registryKey = registryKey;
 		this.mod_id = modid;
-		this.entires = new ArrayList<>();
 	}
 
 //    public <U extends T> RegistryObject<U> register(String id, Supplier<U> entry) {
@@ -26,39 +32,87 @@ public class LazyRegistrar<T> {
 //    }
 
 	public static <R> LazyRegistrar<R> create(Registry<R> registry, String id) {
+		return new LazyRegistrar<>(registry.key(), id);
+	}
+
+	public static <B> LazyRegistrar<B> create(ResourceKey<? extends Registry<B>> registry, String id) {
 		return new LazyRegistrar<>(registry, id);
 	}
 
-	public <R extends T> RegistryObject<R> register(String id, Supplier<R> entry) {
+	public static <B> LazyRegistrar<B> create(ResourceLocation registryName, String id) {
+		return new LazyRegistrar<>(ResourceKey.createRegistryKey(registryName), id);
+	}
+
+	private Supplier<Registry<T>> cachedHolder;
+
+	public Supplier<Registry<T>> makeRegistry() {
+		if (cachedHolder == null)
+			cachedHolder = new RegistryHolder<>(getRegistryKey());
+		return cachedHolder;
+	}
+
+	public <R extends T> RegistryObject<R> register(String id, Supplier<? extends R> entry) {
 		return register(new ResourceLocation(mod_id, id), entry);
 	}
 
-	public <R extends T> RegistryObject<R> register(ResourceLocation id, Supplier<R> entry) {
-		RegistryObject<R> obj = new RegistryObject<>(id, entry, ResourceKey.create(registry.key(), id));
-		entires.add(obj);
-		return obj;
+	public <R extends T> RegistryObject<R> register(ResourceLocation id, final Supplier<? extends R> entry) {
+		RegistryObject<? extends R> obj = new RegistryObject<>(id, entry, ResourceKey.create(getRegistryKey(), id));
+		if (entries.putIfAbsent((RegistryObject<T>) obj, entry) != null) {
+			throw new IllegalArgumentException("Duplicate registration " + id);
+		}
+		return (RegistryObject<R>) obj;
 	}
-
-	private Consumer<RegistryObject<? extends T>> callback;
 
 	public void register() {
-		entires.forEach(entry -> {
+		Registry<T> registry = makeRegistry().get();
+		entries.forEach((entry, sup) -> {
 			Registry.register(registry, entry.getId(), entry.get());
-			if(callback != null)
-				callback.accept(entry);
 		});
-		entires.forEach(entry -> entry.setWrappedEntry(() -> registry.get(entry.getId())));
+		entries.forEach((entry, sup) -> entry.setWrappedEntry(() -> registry.get(entry.getId())));
 	}
 
-	public void setRegistryCallback(Consumer<RegistryObject<? extends T>> callback) {
-		this.callback = callback;
-	}
-
-	public <B extends Block> RegistryObject register(String name, T b) {
+	public <B extends Block> RegistryObject<T> register(String name, T b) {
 		return register(name, () -> b);
 	}
 
-	public List<RegistryObject<? extends T>> getEntires() {
-		return entires;
+	public Collection<RegistryObject<T>> getEntries() {
+		return entriesView;
+	}
+
+	/**
+	 * @return The registry key stored in this deferred register. Useful for creating new deferred registers based on an existing one.
+	 */
+	public ResourceKey<? extends Registry<T>> getRegistryKey() {
+		return this.registryKey;
+	}
+
+	private static class RegistryHolder<V> implements Supplier<Registry<V>> {
+		private final ResourceKey<? extends Registry<V>> registryKey;
+		private Registry<V> registry = null;
+		private final List<Registry<? extends Registry<?>>> registries;
+
+		private RegistryHolder(ResourceKey<? extends Registry<V>> registryKey) {
+			this.registryKey = registryKey;
+			this.registries = new ArrayList<>();
+			registerRegistry(Registry.REGISTRY);
+			registerRegistry(BuiltinRegistries.REGISTRY);
+		}
+
+		public void registerRegistry(Registry<? extends Registry<?>> registry) {
+			registries.add(registry);
+		}
+
+		@Override
+		public Registry<V> get() {
+			// Keep looking up the registry until it's not null
+			registries.forEach(reg -> {
+				if (reg.containsKey(registryKey.location()))
+					this.registry = (Registry<V>) reg.get(registryKey.location());
+			});
+			if (this.registry == null)
+				this.registry = (Registry<V>) FabricRegistryBuilder.createSimple(null, registryKey.location()).buildAndRegister();
+
+			return this.registry;
+		}
 	}
 }
