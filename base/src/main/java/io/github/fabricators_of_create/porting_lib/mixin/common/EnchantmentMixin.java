95c06a1d79d8a55f61d80bbb4ac6e9e5e2ef6580
TropheusJ <jverrellijr@icloud.com>
2022-12-11 14:52:24 -0500

    so close to a perfect merge

--- base/src/main/java/io/github/fabricators_of_create/porting_lib/mixin/common/EnchantmentMixin.java
+++ base/src/main/java/io/github/fabricators_of_create/porting_lib/mixin/common/EnchantmentMixin.java
@@ -1,5 +1,7 @@
 package io.github.fabricators_of_create.porting_lib.mixin.common;
 
+import io.github.fabricators_of_create.porting_lib.enchant.CustomEnchantingBehaviorItem;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
