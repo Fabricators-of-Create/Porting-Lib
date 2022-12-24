91db4523336449cee8dee32b582bfbaf08ba56a1
TropheusJ <jverrellijr@icloud.com>
2022-12-24 18:09:40 -0500

    EnchantmentHelper properly supports custom enchantments

--- src/main/java/io/github/fabricators_of_create/porting_lib/mixin/common/EnchantmentMixin.java
+++ src/main/java/io/github/fabricators_of_create/porting_lib/mixin/common/EnchantmentMixin.java
@@ -1,5 +1,7 @@
 package io.github.fabricators_of_create.porting_lib.mixin.common;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+
 import io.github.fabricators_of_create.porting_lib.enchant.CustomEnchantingBehaviorItem;
 import io.github.fabricators_of_create.porting_lib.enchant.CustomEnchantingTableBehaviorEnchantment;
 
@@ -8,6 +10,9 @@ import io.github.fabricators_of_create.porting_lib.extensions.RegistryNameProvid
 import io.github.fabricators_of_create.porting_lib.util.MixinHelper;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
+
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.enchantment.EnchantmentCategory;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -20,6 +25,10 @@ import net.minecraft.world.item.enchantment.Enchantment;
 
 @Mixin(Enchantment.class)
 public abstract class EnchantmentMixin implements RegistryNameProvider {
+	/**
+	 * Same behavior as {@link EnchantmentHelperMixin#port_lib$customEnchantability(EnchantmentCategory, Item, Operation, int, ItemStack, boolean)}
+	 */
+	@SuppressWarnings("JavadocReference")
 	@Inject(method = "canEnchant", at = @At("HEAD"), cancellable = true)
 	private void port_lib$canEnchant(ItemStack itemStack, CallbackInfoReturnable<Boolean> cir) {
 		if (this instanceof CustomEnchantingTableBehaviorEnchantment custom) {
