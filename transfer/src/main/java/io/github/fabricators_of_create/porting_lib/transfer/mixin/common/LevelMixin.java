515bb6979455c2075225b91ccbbdda3b978b846e
TropheusJ <jverrellijr@icloud.com>
2022-12-30 22:32:34 -0500

    add StorageProvider, deprecate additional caches

--- transfer/src/main/java/io/github/fabricators_of_create/porting_lib/transfer/mixin/common/LevelMixin.java
+++ transfer/src/main/java/io/github/fabricators_of_create/porting_lib/transfer/mixin/common/LevelMixin.java
@@ -1,10 +1,29 @@
 package io.github.fabricators_of_create.porting_lib.transfer.mixin.common;
 
 import io.github.fabricators_of_create.porting_lib.extensions.transfer.LevelExtensions;
+import io.github.fabricators_of_create.porting_lib.transfer.cache.EmptyFluidLookupCache;
+import io.github.fabricators_of_create.porting_lib.transfer.cache.EmptyItemLookupCache;
+import net.fabricmc.fabric.api.lookup.v1.block.BlockApiCache;
+import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariant;
+import net.fabricmc.fabric.api.transfer.v1.item.ItemVariant;
+import net.fabricmc.fabric.api.transfer.v1.storage.Storage;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.level.Level;
 
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(Level.class)
-public class LevelMixin implements LevelExtensions {
+public abstract class LevelMixin implements LevelExtensions {
+	@Override
+	public BlockApiCache<Storage<ItemVariant>, Direction> port_lib$getItemCache(BlockPos pos) {
+		// uh oh. Not a ClientLevel or ServerLevel!
+		return EmptyItemLookupCache.INSTANCE;
+	}
+
+	@Override
+	public BlockApiCache<Storage<FluidVariant>, Direction> port_lib$getFluidApiCache(BlockPos pos) {
+		// uh oh. Not a ClientLevel or ServerLevel!
+		return EmptyFluidLookupCache.INSTANCE;
+	}
 }
---
+++ transfer/src/main/java/io/github/fabricators_of_create/porting_lib/util/StorageProvider.java
@@ -0,0 +1,95 @@
+package io.github.fabricators_of_create.porting_lib.util;
+
+import net.fabricmc.fabric.api.lookup.v1.block.BlockApiCache;
+import net.fabricmc.fabric.api.lookup.v1.block.BlockApiLookup;
+import net.fabricmc.fabric.api.transfer.v1.fluid.FluidStorage;
+import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariant;
+import net.fabricmc.fabric.api.transfer.v1.item.ItemStorage;
+import net.fabricmc.fabric.api.transfer.v1.item.ItemVariant;
+import net.fabricmc.fabric.api.transfer.v1.storage.Storage;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.Level;
+
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.BiPredicate;
+import java.util.function.Function;
+
+public class StorageProvider<T> implements Function<Direction, Storage<T>> {
+	public final BlockApiLookup<Storage<T>, Direction> lookup;
+	public final Level level;
+	public final BlockPos pos;
+
+	protected StorageProvider(BlockApiLookup<Storage<T>, Direction> lookup, Level level, BlockPos pos) {
+		this.lookup = lookup;
+		this.level = level;
+		this.pos = pos;
+	}
+
+	@Nullable
+	public Storage<T> get(Direction direction) {
+		return lookup.find(level, pos, direction);
+	}
+
+	@Override
+	@Nullable
+	public Storage<T> apply(Direction direction) {
+		return get(direction);
+	}
+
+	public StorageProvider<T> filter(BiPredicate<StorageProvider<T>, Storage<T>> filter) {
+		return new FilteringStorageProvider<>(this, filter);
+	}
+
+	public static StorageProvider<FluidVariant> createForFluids(Level level, BlockPos pos) {
+		return create(FluidStorage.SIDED, level, pos);
+	}
+
+	public static StorageProvider<ItemVariant> createForItems(Level level, BlockPos pos) {
+		return create(ItemStorage.SIDED, level, pos);
+	}
+
+	public static <T> StorageProvider<T> create(BlockApiLookup<Storage<T>, Direction> lookup, Level level, BlockPos pos) {
+		if (level instanceof ServerLevel server) {
+			return new CachedStorageProvider<>(lookup, server, pos);
+		}
+		return new StorageProvider<>(lookup, level, pos);
+	}
+
+	protected static class CachedStorageProvider<T> extends StorageProvider<T> {
+		private final BlockApiCache<Storage<T>, Direction> cache;
+
+		public CachedStorageProvider(BlockApiLookup<Storage<T>, Direction> lookup, ServerLevel level, BlockPos pos) {
+			super(lookup, level, pos);
+			this.cache = BlockApiCache.create(lookup, level, pos);
+		}
+
+		@Override
+		public Storage<T> get(Direction direction) {
+			return cache.find(direction);
+		}
+	}
+
+	protected static class FilteringStorageProvider<T> extends StorageProvider<T> {
+		private final StorageProvider<T> wrapped;
+		private final BiPredicate<StorageProvider<T>, Storage<T>> filter;
+
+		protected FilteringStorageProvider(StorageProvider<T> wrapped, BiPredicate<StorageProvider<T>, Storage<T>> filter) {
+			super(wrapped.lookup, wrapped.level, wrapped.pos);
+			this.wrapped = wrapped;
+			this.filter = filter;
+		}
+
+		@Override
+		@Nullable
+		public Storage<T> get(Direction direction) {
+			Storage<T> storage = super.get(direction);
+			if (filter.test(this, storage)) {
+				return storage;
+			}
+			return null;
+		}
+	}
+}
