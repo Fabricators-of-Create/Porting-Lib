b7de4337507131d6bb9d1871252191d267741dda
iczero <iczero4@gmail.com>
2023-01-11 19:00:25 -0500

    don't throw in LevelExtensions default impl
    fixes crash with litematica client-side schematic rendering

--- transfer/src/main/java/io/github/fabricators_of_create/porting_lib/extensions/transfer/LevelExtensions.java
+++ transfer/src/main/java/io/github/fabricators_of_create/porting_lib/extensions/transfer/LevelExtensions.java
@@ -1,5 +1,7 @@
 package io.github.fabricators_of_create.porting_lib.extensions.transfer;
 
+import io.github.fabricators_of_create.porting_lib.transfer.cache.EmptyFluidLookupCache;
+import io.github.fabricators_of_create.porting_lib.transfer.cache.EmptyItemLookupCache;
 import net.fabricmc.fabric.api.lookup.v1.block.BlockApiCache;
 import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariant;
 import net.fabricmc.fabric.api.transfer.v1.item.ItemVariant;
@@ -9,10 +11,11 @@ import net.minecraft.core.Direction;
 
 public interface LevelExtensions {
 	default BlockApiCache<Storage<ItemVariant>, Direction> port_lib$getItemCache(BlockPos pos) {
-		throw new RuntimeException("this should be overridden via mixin. what?");
+		// litematica compat: don't throw, just return empty
+		return EmptyItemLookupCache.INSTANCE;
 	}
 
 	default BlockApiCache<Storage<FluidVariant>, Direction> port_lib$getFluidApiCache(BlockPos pos) {
-		throw new RuntimeException("this should be overridden via mixin. what?");
+		return EmptyFluidLookupCache.INSTANCE;
 	}
 }
