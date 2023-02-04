d92d9d458a40c0b5009574aaaa090505d7bfd8a6
AlphaMode <alphamodeyt@gmail.com>
2023-02-03 20:55:08 -0600

    wip Model builder stuff

---
+++ model_generators/src/main/java/io/github/fabricators_of_create/porting_lib/models/generators/CustomLoaderBuilder.java
@@ -0,0 +1,49 @@
+package io.github.fabricators_of_create.porting_lib.models.generators;
+
+import com.google.common.base.Preconditions;
+import com.google.gson.JsonObject;
+
+import io.github.fabricators_of_create.porting_lib.data.ExistingFileHelper;
+import net.minecraft.resources.ResourceLocation;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public abstract class CustomLoaderBuilder<T extends ModelBuilder<T>> {
+	protected final ResourceLocation loaderId;
+	protected final T parent;
+	protected final ExistingFileHelper existingFileHelper;
+	protected final Map<String, Boolean> visibility = new LinkedHashMap<>();
+
+	protected CustomLoaderBuilder(ResourceLocation loaderId, T parent, ExistingFileHelper existingFileHelper) {
+		this.loaderId = loaderId;
+		this.parent = parent;
+		this.existingFileHelper = existingFileHelper;
+	}
+
+	public CustomLoaderBuilder<T> visibility(String partName, boolean show) {
+		Preconditions.checkNotNull(partName, "partName must not be null");
+		this.visibility.put(partName, show);
+		return this;
+	}
+
+	public T end() {
+		return parent;
+	}
+
+	public JsonObject toJson(JsonObject json) {
+		json.addProperty("loader", loaderId.toString());
+
+		if (visibility.size() > 0) {
+			JsonObject visibilityObj = new JsonObject();
+
+			for(Map.Entry<String, Boolean> entry : visibility.entrySet()) {
+				visibilityObj.addProperty(entry.getKey(), entry.getValue());
+			}
+
+			json.add("visibility", visibilityObj);
+		}
+
+		return json;
+	}
+}
