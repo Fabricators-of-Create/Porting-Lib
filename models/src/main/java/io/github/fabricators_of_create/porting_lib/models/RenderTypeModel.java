45d51c43406657c99e3a3997f60354c23925f4f8
AlphaMode <alphamodeyt@gmail.com>
2023-02-10 23:49:58 -0600

    undo derp

--- models/src/main/java/io/github/fabricators_of_create/porting_lib/models/RenderTypeModel.java
+++ models/src/main/java/io/github/fabricators_of_create/porting_lib/models/RenderTypeModel.java
@@ -1,20 +1,19 @@
 package io.github.fabricators_of_create.porting_lib.models;
 
+import java.util.function.Supplier;
+
 import net.fabricmc.fabric.api.renderer.v1.RendererAccess;
 import net.fabricmc.fabric.api.renderer.v1.material.BlendMode;
 import net.fabricmc.fabric.api.renderer.v1.material.RenderMaterial;
 import net.fabricmc.fabric.api.renderer.v1.model.ForwardingBakedModel;
 import net.fabricmc.fabric.api.renderer.v1.render.RenderContext;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.core.BlockPos;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockAndTintGetter;
 import net.minecraft.world.level.block.state.BlockState;
-
-import java.util.function.Supplier;
 
 public class RenderTypeModel extends ForwardingBakedModel {
 	protected final RenderMaterial material;
@@ -26,10 +25,20 @@ public class RenderTypeModel extends ForwardingBakedModel {
 	@Override
 	public void emitBlockQuads(BlockAndTintGetter blockView, BlockState state, BlockPos pos, Supplier<RandomSource> randomSupplier, RenderContext context) {
 		context.pushTransform(quad -> {
-			quad.material(RendererAccess.INSTANCE.getRenderer().materialFinder().blendMode(0, BlendMode.fromRenderLayer(ItemBlockRenderTypes.getRenderType(state, Minecraft.useFancyGraphics()))).find());
+			quad.material(this.material);
 			return true;
 		});
 		super.emitBlockQuads(blockView, state, pos, randomSupplier, context);
+		context.popTransform();
+	}
+
+	@Override
+	public void emitItemQuads(ItemStack stack, Supplier<RandomSource> randomSupplier, RenderContext context) {
+		context.pushTransform(quad -> {
+			quad.material(this.material);
+			return true;
+		});
+		super.emitItemQuads(stack, randomSupplier, context);
 		context.popTransform();
 	}
 }
