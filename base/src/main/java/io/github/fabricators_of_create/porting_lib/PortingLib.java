1d462d8f5bb231a7247525252ff94bc5bee709f4
alpha <alphamodeyt@gmail.com>
2023-04-11 22:43:56 -0500

    Add ModIdArgument

--- base/src/main/java/io/github/fabricators_of_create/porting_lib/PortingLib.java
+++ base/src/main/java/io/github/fabricators_of_create/porting_lib/PortingLib.java
@@ -1,16 +1,6 @@
 package io.github.fabricators_of_create.porting_lib;
 
-import io.github.fabricators_of_create.porting_lib.loot.LootModifierManager;
-
-import net.minecraft.world.item.ItemStack;
-
-import net.minecraft.world.item.Items;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import io.github.fabricators_of_create.porting_lib.attributes.PortingLibAttributes;
-import io.github.fabricators_of_create.porting_lib.crafting.CraftingHelper;
+import io.github.fabricators_of_create.porting_lib.command.ModIdArgument;
 import io.github.fabricators_of_create.porting_lib.data.ConditionalRecipe;
 import io.github.fabricators_of_create.porting_lib.loot.CanToolPerformAction;
 import io.github.fabricators_of_create.porting_lib.loot.LootModifierManager;
@@ -19,13 +9,14 @@ import io.github.fabricators_of_create.porting_lib.transfer.item.ItemItemStorage
 import io.github.fabricators_of_create.porting_lib.util.PortingHooks;
 import io.github.fabricators_of_create.porting_lib.util.TierSortingRegistry;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.commands.synchronization.ArgumentTypeInfos;
+import net.minecraft.commands.synchronization.SingletonArgumentInfo;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 
-import java.util.Arrays;
-
 public class PortingLib implements ModInitializer {
+
 	@Override
 	public void onInitialize() {
 		TierSortingRegistry.init();
@@ -38,5 +29,8 @@ public class PortingLib implements ModInitializer {
 
 		Registry.register(BuiltInRegistries.LOOT_CONDITION_TYPE, new ResourceLocation("forge:loot_table_id"), LootTableIdCondition.LOOT_TABLE_ID);
 		Registry.register(BuiltInRegistries.LOOT_CONDITION_TYPE, new ResourceLocation("forge:can_tool_perform_action"), CanToolPerformAction.LOOT_CONDITION_TYPE);
+
+		ArgumentTypeInfos.register(BuiltInRegistries.COMMAND_ARGUMENT_TYPE, PortingConstants.id("modid").toString(), ModIdArgument.class,
+				SingletonArgumentInfo.contextFree(ModIdArgument::modIdArgument));
 	}
 }
