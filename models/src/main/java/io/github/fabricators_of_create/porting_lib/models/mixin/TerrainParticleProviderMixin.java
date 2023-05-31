5e775756ba30de4095997d905c55b6d3a6e6ddf6
alpha <alphamodeyt@gmail.com>
2023-05-31 18:08:32 -0500

    Supporting modem wrapping on CustomParticleIconModel. Fix invalid class reference on RenderArmCallback

--- models/src/main/java/io/github/fabricators_of_create/porting_lib/models/mixin/TerrainParticleProviderMixin.java
+++ models/src/main/java/io/github/fabricators_of_create/porting_lib/models/mixin/TerrainParticleProviderMixin.java
@@ -1,22 +1,20 @@
 package io.github.fabricators_of_create.porting_lib.models.mixin;
 
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import io.github.fabricators_of_create.porting_lib.model.CustomParticleIconModel;
 import io.github.fabricators_of_create.porting_lib.models.extensions.BlockParticleOptionExtensions;
+import net.fabricmc.fabric.api.renderer.v1.model.WrapperBakedModel;
 import net.fabricmc.fabric.api.rendering.data.v1.RenderAttachedBlockView;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.particle.Particle;
 import net.minecraft.client.particle.TerrainParticle;
-
 import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.core.particles.BlockParticleOption;
-
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(TerrainParticle.Provider.class)
 public class TerrainParticleProviderMixin {
@@ -24,6 +22,8 @@ public class TerrainParticleProviderMixin {
 	private void setCustomParticleIcon(BlockParticleOption type, ClientLevel level, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed, CallbackInfoReturnable<Particle> cir) {
 		BakedModel model = Minecraft.getInstance().getBlockRenderer().getBlockModelShaper().getBlockModel(type.getState());
 		if (model instanceof CustomParticleIconModel particleIconModel)
+			((TextureSheetParticleAccessor)cir.getReturnValue()).callSetSprite(particleIconModel.getParticleIcon(((RenderAttachedBlockView) level).getBlockEntityRenderAttachment(((BlockParticleOptionExtensions)type).getPos())));
+		else if (model instanceof WrapperBakedModel wrapped && wrapped.getWrappedModel() instanceof CustomParticleIconModel particleIconModel)
 			((TextureSheetParticleAccessor)cir.getReturnValue()).callSetSprite(particleIconModel.getParticleIcon(((RenderAttachedBlockView) level).getBlockEntityRenderAttachment(((BlockParticleOptionExtensions)type).getPos())));
 	}
 }
