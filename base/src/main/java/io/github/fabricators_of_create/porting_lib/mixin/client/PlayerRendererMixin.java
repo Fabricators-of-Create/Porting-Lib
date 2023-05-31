e369e425dd76f053e5979622bbc9ce80d02a9180
alpha <alphamodeyt@gmail.com>
2023-05-31 01:17:51 -0500

    Backport RenderArmCallback

--- base/src/main/java/io/github/fabricators_of_create/porting_lib/mixin/client/PlayerRendererMixin.java
+++ base/src/main/java/io/github/fabricators_of_create/porting_lib/mixin/client/PlayerRendererMixin.java
@@ -2,10 +2,13 @@ package io.github.fabricators_of_create.porting_lib.mixin.client;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 
+import io.github.fabricators_of_create.porting_lib.event.client.RenderArmCallback;
 import io.github.fabricators_of_create.porting_lib.event.client.RenderPlayerEvents;
 import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.player.PlayerRenderer;
+
+import net.minecraft.world.entity.HumanoidArm;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -23,5 +26,17 @@ public class PlayerRendererMixin {
 	@Inject(method = "render(Lnet/minecraft/client/player/AbstractClientPlayer;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V", at = @At("RETURN"))
 	private void port_lib$onRenderPlayerPost(AbstractClientPlayer player, float f, float partialTick, PoseStack poseStack, MultiBufferSource buffer, int packedLight, CallbackInfo ci) {
 		RenderPlayerEvents.POST.invoker().onPostRenderPlayer(player, (PlayerRenderer) (Object) this, partialTick, poseStack, buffer, packedLight);
+	}
+
+	@Inject(method = "renderLeftHand", at = @At("HEAD"), cancellable = true)
+	private void onRenderLeftArm(PoseStack poseStack, MultiBufferSource buffer, int packedLight, AbstractClientPlayer player, CallbackInfo ci) {
+		if (RenderArmCallback.EVENT.invoker().onRenderArm(poseStack, buffer, packedLight, player, HumanoidArm.LEFT))
+			ci.cancel();
+	}
+
+	@Inject(method = "renderRightHand", at = @At("HEAD"), cancellable = true)
+	private void onRenderRightArm(PoseStack poseStack, MultiBufferSource buffer, int packedLight, AbstractClientPlayer player, CallbackInfo ci) {
+		if (RenderArmCallback.EVENT.invoker().onRenderArm(poseStack, buffer, packedLight, player, HumanoidArm.RIGHT))
+			ci.cancel();
 	}
 }
