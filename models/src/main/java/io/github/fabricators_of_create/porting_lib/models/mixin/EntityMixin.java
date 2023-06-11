675ad471a08a74cfdb5d99ea08a78da032f23fe4
TropheusJ <jverrellijr@icloud.com>
2023-06-11 19:23:50 -0400

    improve ScreenEffectRendererMixin

--- models/src/main/java/io/github/fabricators_of_create/porting_lib/models/mixin/EntityMixin.java
+++ models/src/main/java/io/github/fabricators_of_create/porting_lib/models/mixin/EntityMixin.java
@@ -2,20 +2,15 @@ package io.github.fabricators_of_create.porting_lib.models.mixin;
 
 import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.phys.Vec3;
 
 import io.github.fabricators_of_create.porting_lib.models.extensions.BlockParticleOptionExtensions;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(Entity.class)
 public abstract class EntityMixin {
-	@Shadow
-	public abstract Vec3 position();
-
 	@ModifyArg(
 			method = "spawnSprintParticle",
 			at = @At(
