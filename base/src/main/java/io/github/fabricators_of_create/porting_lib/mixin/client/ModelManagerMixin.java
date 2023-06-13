3fb0095652f9bffe4799cf30e9c0753fbc6b7e43
TropheusJ <jverrellijr@icloud.com>
2023-06-12 23:35:26 -0400

    improve ModelManagerMixin

--- base/src/main/java/io/github/fabricators_of_create/porting_lib/mixin/client/ModelManagerMixin.java
+++ base/src/main/java/io/github/fabricators_of_create/porting_lib/mixin/client/ModelManagerMixin.java
@@ -1,32 +1,38 @@
 package io.github.fabricators_of_create.porting_lib.mixin.client;
 
-import java.util.Map;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+
+import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.client.resources.model.ModelBakery;
+import net.minecraft.resources.ResourceLocation;
+
+import net.minecraft.server.packs.resources.ResourceManager;
+import net.minecraft.util.profiling.ProfilerFiller;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import io.github.fabricators_of_create.porting_lib.event.client.ModelsBakedCallback;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.resources.model.BakedModel;
-import net.minecraft.client.resources.model.ModelBakery;
 import net.minecraft.client.resources.model.ModelManager;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.packs.resources.ResourceManager;
-import net.minecraft.util.profiling.ProfilerFiller;
+
+import java.util.Map;
 
 @Environment(EnvType.CLIENT)
 @Mixin(ModelManager.class)
 public abstract class ModelManagerMixin {
-	@Shadow
-	private Map<ResourceLocation, BakedModel> bakedRegistry;
-
-	@Inject(method = "apply", at = @At(value = "INVOKE_STRING", target = "Lnet/minecraft/util/profiling/ProfilerFiller;popPush(Ljava/lang/String;)V", args = "ldc=cache", shift = At.Shift.BEFORE))
-	public void port_lib$onModelBake(ModelBakery modelLoader, ResourceManager resourceManager, ProfilerFiller profiler, CallbackInfo ci) {
-		ModelsBakedCallback.EVENT.invoker().onModelsBaked((ModelManager) (Object) this, bakedRegistry, modelLoader);
+	@ModifyExpressionValue(
+			method = "apply(Lnet/minecraft/client/resources/model/ModelBakery;Lnet/minecraft/server/packs/resources/ResourceManager;Lnet/minecraft/util/profiling/ProfilerFiller;)V",
+			at = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/client/resources/model/ModelBakery;getBakedTopLevelModels()Ljava/util/Map;"
+			)
+	)
+	public Map<ResourceLocation, BakedModel> fireModelBakeEvent(Map<ResourceLocation, BakedModel> models,
+									 ModelBakery bakery, ResourceManager resourceManager, ProfilerFiller profiler) {
+		ModelsBakedCallback.EVENT.invoker().onModelsBaked((ModelManager) (Object) this, models, bakery);
+		return models;
 	}
 }
