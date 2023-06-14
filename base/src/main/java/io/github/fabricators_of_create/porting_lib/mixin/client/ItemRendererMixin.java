8e2f02ae7a271f4e9ba736545fca974550dd4b7e
TropheusJ <jverrellijr@icloud.com>
2023-06-14 00:18:39 -0400

    use maybeApplyTransform here too

--- base/src/main/java/io/github/fabricators_of_create/porting_lib/mixin/client/ItemRendererMixin.java
+++ base/src/main/java/io/github/fabricators_of_create/porting_lib/mixin/client/ItemRendererMixin.java
@@ -1,19 +1,6 @@
 package io.github.fabricators_of_create.porting_lib.mixin.client;
 
-import com.mojang.blaze3d.vertex.VertexConsumer;
-
-import com.mojang.math.Matrix3f;
-import com.mojang.math.Matrix4f;
-
-import io.github.fabricators_of_create.porting_lib.extensions.Matrix3fExtensions;
-import io.github.fabricators_of_create.porting_lib.extensions.Matrix4fExtensions;
-import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
-import net.minecraft.client.renderer.ItemModelShaper;
-import net.minecraft.client.renderer.RenderType;
-
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
@@ -23,19 +10,19 @@ import io.github.fabricators_of_create.porting_lib.render.TransformTypeDependent
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.block.model.ItemTransforms;
+import net.minecraft.client.renderer.block.model.ItemTransforms.TransformType;
 import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.world.item.ItemStack;
 
 @Environment(EnvType.CLIENT)
-@Mixin(value = ItemRenderer.class, priority = 10000)
+@Mixin(value = ItemRenderer.class, priority = 1200)
 public abstract class ItemRendererMixin {
 	// FIXME CANVAS COMPAT
 	@ModifyVariable(method = "render", at = @At("HEAD"), argsOnly = true)
-	private BakedModel port_lib$handleModel(BakedModel model, ItemStack itemStack, ItemTransforms.TransformType transformType, boolean leftHand, PoseStack matrixStack, MultiBufferSource buffer, int combinedLight, int combinedOverlay, BakedModel model1) {
-		if (model instanceof TransformTypeDependentItemBakedModel handler)
-			return handler.applyTransform(transformType, matrixStack, leftHand);
-		return model;
+	private BakedModel applyCustomTransforms(BakedModel model, ItemStack itemStack, TransformType transformType,
+											 boolean leftHand, PoseStack matrixStack, MultiBufferSource buffer,
+											 int combinedLight, int combinedOverlay, BakedModel model1) {
+		return TransformTypeDependentItemBakedModel.maybeApplyTransform(model, transformType, matrixStack, leftHand);
 	}
 }
