a7958e265b532b36bc1f519c506b914c14392fd4
TropheusJ <jverrellijr@icloud.com>
2023-06-13 22:40:26 -0400

    maybeApplyTransform

--- model_loader/src/main/java/io/github/fabricators_of_create/porting_lib/render/TransformTypeDependentItemBakedModel.java
+++ model_loader/src/main/java/io/github/fabricators_of_create/porting_lib/render/TransformTypeDependentItemBakedModel.java
@@ -2,9 +2,7 @@ package io.github.fabricators_of_create.porting_lib.render;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 
-import com.mojang.math.Transformation;
-
-import io.github.fabricators_of_create.porting_lib.util.TransformationHelper;
+import net.fabricmc.fabric.api.renderer.v1.model.WrapperBakedModel;
 import net.minecraft.client.renderer.block.model.ItemTransforms;
 import net.minecraft.client.renderer.block.model.ItemTransforms.TransformType;
 import net.minecraft.client.resources.model.BakedModel;
@@ -17,5 +15,26 @@ public interface TransformTypeDependentItemBakedModel {
 	default BakedModel applyTransform(ItemTransforms.TransformType transformType, PoseStack poseStack, boolean applyLeftHandTransform) {
 		((BakedModel) this).getTransforms().getTransform(transformType).apply(applyLeftHandTransform, poseStack);
 		return (BakedModel) this;
+	}
+
+	/**
+	 * Attempt to apply a custom transform from the given model, unwrapping wrappers if needed.
+	 * Does nothing if not a {@link TransformTypeDependentItemBakedModel}.
+	 */
+	static BakedModel maybeApplyTransform(BakedModel model, TransformType type, PoseStack poseStack, boolean leftHand) {
+		if (model instanceof TransformTypeDependentItemBakedModel transformer)
+			return transformer.applyTransform(type, poseStack, leftHand);
+
+		BakedModel wrapped = model;
+		while (wrapped instanceof WrapperBakedModel wrapper) {
+			wrapped = wrapper.getWrappedModel();
+			if (wrapped == null) {
+				return model;
+			} else if (wrapped instanceof TransformTypeDependentItemBakedModel transformer) {
+				return transformer.applyTransform(type, poseStack, leftHand);
+			}
+		}
+
+		return model;
 	}
 }
