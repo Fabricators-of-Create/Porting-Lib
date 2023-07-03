4adfea0c35b0ff1ae0de5a6676904f380e2c4629
TropheusJ <jverrellijr@icloud.com>
2023-07-03 05:02:36 -0400

    fix and clean up frex compat

--- models/src/main/java/io/github/fabricators_of_create/porting_lib/models/mixin/PortingLibModelsPlugin.java
+++ models/src/main/java/io/github/fabricators_of_create/porting_lib/models/mixin/PortingLibModelsPlugin.java
@@ -10,15 +10,6 @@ import java.util.List;
 import java.util.Set;
 
 public class PortingLibModelsPlugin implements IMixinConfigPlugin {
-	@Override
-	public void onLoad(String mixinPackage) {
-
-	}
-
-	@Override
-	public String getRefMapperConfig() {
-		return null;
-	}
 
 	@Override
 	public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
@@ -30,8 +21,17 @@ public class PortingLibModelsPlugin implements IMixinConfigPlugin {
 	}
 
 	@Override
-	public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+	public void onLoad(String mixinPackage) {
+	}
 
+	@Override
+	public String getRefMapperConfig() {
+		return null;
+	}
+
+
+	@Override
+	public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
 	}
 
 	@Override
@@ -41,11 +41,9 @@ public class PortingLibModelsPlugin implements IMixinConfigPlugin {
 
 	@Override
 	public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
-
 	}
 
 	@Override
 	public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
-
 	}
 }
--- models/src/main/java/io/github/fabricators_of_create/porting_lib/models/mixin/client/frex/ItemRenderContextMixin.java
+++ models/src/main/java/io/github/fabricators_of_create/porting_lib/models/mixin/client/frex/ItemRenderContextMixin.java
@@ -14,33 +14,58 @@ import io.github.fabricators_of_create.porting_lib.models.TransformTypeDependent
 import io.vram.frex.api.buffer.QuadSink;
 import io.vram.frex.api.model.ItemModel;
 import io.vram.frex.base.renderer.context.render.ItemRenderContext;
+import net.minecraft.client.renderer.ItemModelShaper;
+import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.block.model.ItemTransform;
 import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.world.item.ItemDisplayContext;
+import net.minecraft.world.item.ItemStack;
 
 @Mixin(ItemRenderContext.class)
 public class ItemRenderContextMixin {
-	@WrapWithCondition(method = "renderItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/block/model/ItemTransform;apply(ZLcom/mojang/blaze3d/vertex/PoseStack;)V"))
-	private boolean hasCustomTransform(ItemTransform transform, boolean leftHanded, PoseStack poseStack, @Local(index = 3) ItemDisplayContext transformType, @Local(index = 9) BakedModel model, @Share("tranformed") LocalRef<BakedModel> tranformed) {
+	@WrapWithCondition(
+			method = "renderItem",
+			at = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/client/renderer/block/model/ItemTransform;apply(ZLcom/mojang/blaze3d/vertex/PoseStack;)V"
+			)
+	)
+	private boolean hasCustomTransform(ItemTransform transform, boolean leftHanded, PoseStack poseStack,
+									   ItemModelShaper models, ItemStack stack, ItemDisplayContext renderMode,
+									   boolean isLeftHand, PoseStack poseStack2, MultiBufferSource vertexConsumers,
+									   int light, int overlay, BakedModel model, @Share("transformed") LocalRef<BakedModel> transformed) {
 		if (model instanceof TransformTypeDependentItemBakedModel transformModel) {
-			tranformed.set(transformModel.applyTransform(transformType, poseStack, leftHanded));
+			transformed.set(transformModel.applyTransform(renderMode, poseStack, leftHanded));
 			return false;
 		}
-		tranformed.set(null);
+		transformed.set(null);
 		return true;
 	}
 
-	@ModifyReceiver(method = "renderItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/resources/model/BakedModel;isCustomRenderer()Z"))
-	private BakedModel useTransformedModel(BakedModel model, @Share("tranformed") LocalRef<BakedModel> tranformed) {
-		BakedModel newModel = tranformed.get();
+	@ModifyReceiver(
+			method = "renderItem",
+			at = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/client/resources/model/BakedModel;isCustomRenderer()Z"
+			)
+	)
+	private BakedModel useTransformedModel(BakedModel model, @Share("transformed") LocalRef<BakedModel> transformed) {
+		BakedModel newModel = transformed.get();
 		if (newModel != null)
 			return newModel;
 		return model;
 	}
 
-	@ModifyReceiver(method = "renderItem", at = @At(value = "INVOKE", target = "Lio/vram/frex/api/model/ItemModel;renderAsItem(Lio/vram/frex/api/model/ItemModel$ItemInputContext;Lio/vram/frex/api/buffer/QuadSink;)V"))
-	private ItemModel renderTransformedModel(ItemModel model, ItemModel.ItemInputContext input, QuadSink output, @Share("tranformed") LocalRef<BakedModel> tranformed) {
-		BakedModel newModel = tranformed.get();
+	@ModifyReceiver(
+			method = "renderItem",
+			at = @At(
+					value = "INVOKE",
+					target = "Lio/vram/frex/api/model/ItemModel;renderAsItem(Lio/vram/frex/api/model/ItemModel$ItemInputContext;Lio/vram/frex/api/buffer/QuadSink;)V"
+			)
+	)
+	private ItemModel renderTransformedModel(ItemModel model, ItemModel.ItemInputContext input,
+											 QuadSink output, @Share("transformed") LocalRef<BakedModel> transformed) {
+		BakedModel newModel = transformed.get();
 		if (newModel != null)
 			return (ItemModel) newModel;
 		return model;
