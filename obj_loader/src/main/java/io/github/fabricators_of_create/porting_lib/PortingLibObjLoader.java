6c60d1ec433c61ff06d72ee6664693f3fbe80589
TropheusJ <jverrellijr@icloud.com>
2023-07-19 23:42:57 -0500

    migrate obj_loader to model loading v1 also some significant cleanup

--- obj_loader/src/main/java/io/github/fabricators_of_create/porting_lib/PortingLibObjLoader.java
+++ obj_loader/src/main/java/io/github/fabricators_of_create/porting_lib/PortingLibObjLoader.java
@@ -1,15 +1,18 @@
 package io.github.fabricators_of_create.porting_lib;
 
+import java.util.concurrent.CompletableFuture;
+
+import io.github.fabricators_of_create.porting_lib.core.PortingLib;
 import io.github.fabricators_of_create.porting_lib.models.geometry.RegisterGeometryLoadersCallback;
 import io.github.fabricators_of_create.porting_lib.models.obj.ObjLoader;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.model.loading.v1.ModelLoadingPlugin;
+import net.fabricmc.fabric.api.client.model.loading.v1.PreparableModelLoadingPlugin;
 
 public class PortingLibObjLoader implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// OBJ loader can be used for either plain models or block model geometry
-		ModelLoadingPlugin.register(ObjLoader.INSTANCE);
-		RegisterGeometryLoadersCallback.EVENT.register(loaders -> loaders.put(ObjLoader.ID, ObjLoader.INSTANCE));
+		PreparableModelLoadingPlugin.register((manager, executor) -> CompletableFuture.completedFuture(manager), ObjLoader.INSTANCE);
+		RegisterGeometryLoadersCallback.EVENT.register(loaders -> loaders.put(PortingLib.id("obj"), ObjLoader.INSTANCE));
 	}
 }
--- obj_loader/src/main/java/io/github/fabricators_of_create/porting_lib/models/obj/ObjLoader.java
+++ obj_loader/src/main/java/io/github/fabricators_of_create/porting_lib/models/obj/ObjLoader.java
@@ -1,23 +1,35 @@
 package io.github.fabricators_of_create.porting_lib.models.obj;
 
 import java.io.FileNotFoundException;
-import java.util.Collection;
+import java.io.IOException;
 import java.util.Map;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
-import com.google.common.collect.ImmutableMap;
+import javax.naming.spi.Resolver;
+
 import com.google.common.collect.Maps;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 
+import com.google.gson.JsonParser;
+
+import com.google.gson.JsonSyntaxException;
+import com.mojang.datafixers.util.Either;
+
 import io.github.fabricators_of_create.porting_lib.core.PortingLib;
 import io.github.fabricators_of_create.porting_lib.models.geometry.IGeometryLoader;
-import net.fabricmc.fabric.api.resource.IdentifiableResourceReloadListener;
+import io.github.fabricators_of_create.porting_lib.models.obj.ObjModel.ModelSettings;
+import net.fabricmc.fabric.api.client.model.loading.v1.ModelLoadingPlugin.Context;
+import net.fabricmc.fabric.api.client.model.loading.v1.ModelResolver;
+import net.fabricmc.fabric.api.client.model.loading.v1.PreparableModelLoadingPlugin;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.model.ModelBakery;
+import net.minecraft.client.resources.model.UnbakedModel;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.Resource;
 import net.minecraft.server.packs.resources.ResourceManager;
-import net.minecraft.server.packs.resources.ResourceManagerReloadListener;
 import net.minecraft.util.GsonHelper;
 
 import org.jetbrains.annotations.Nullable;
@@ -28,92 +40,86 @@ import org.jetbrains.annotations.Nullable;
  * Allows the user to enable automatic face culling, toggle quad shading, flip UVs, render emissively and specify a
  * {@link ObjMaterialLibrary material library} override.
  */
-public class ObjLoader implements IGeometryLoader<ObjModel>, ResourceManagerReloadListener, IdentifiableResourceReloadListener {
-	public static ObjLoader INSTANCE = new ObjLoader();
+public class ObjLoader implements PreparableModelLoadingPlugin<ResourceManager>, IGeometryLoader<ObjModel> {
+	public static final ObjLoader INSTANCE = new ObjLoader();
+	public static final String OBJ_MARKER = PortingLib.id("obj_marker").toString();
 
-	private final Map<ObjModel.ModelSettings, ObjModel> modelCache = Maps.newConcurrentMap();
+	private final Map<ModelSettings, ObjModel> modelCache = Maps.newConcurrentMap();
 	private final Map<ResourceLocation, ObjMaterialLibrary> materialCache = Maps.newConcurrentMap();
 
-	private ResourceManager manager = Minecraft.getInstance().getResourceManager();
+	private ResourceManager manager;
 
 	@Override
-	public void onResourceManagerReload(ResourceManager resourceManager) {
+	public void onInitializeModelLoader(ResourceManager manager, Context ctx) {
+		this.manager = manager;
+		// called every reload, clear caches
 		modelCache.clear();
 		materialCache.clear();
-		manager = resourceManager;
+
+		findModels(ctx::addModels);
+		ctx.resolveModel().register(new Resolver());
 	}
 
-	public void setManager(ResourceManager manager) {
-		this.manager = manager;
-	}
-
-	@Override
-	public ObjModel read(JsonObject jsonObject, JsonDeserializationContext deserializationContext) {
-		if (!jsonObject.has("model"))
-			throw new JsonParseException("OBJ Loader requires a 'model' key that points to a valid .OBJ model.");
-
-		if (manager == null)
-			manager = Minecraft.getInstance().getResourceManager();
-
-		String modelLocation = jsonObject.get("model").getAsString();
-
-		boolean automaticCulling = GsonHelper.getAsBoolean(jsonObject, "automatic_culling", true);
-		boolean shadeQuads = GsonHelper.getAsBoolean(jsonObject, "shade_quads", true);
-		boolean flipV = GsonHelper.getAsBoolean(jsonObject, "flip_v", false);
-		boolean emissiveAmbient = GsonHelper.getAsBoolean(jsonObject, "emissive_ambient", true);
-		String mtlOverride = GsonHelper.getAsString(jsonObject, "mtl_override", null);
-
-		// TODO: Deprecated names. To be removed in 1.20
-		var deprecationWarningsBuilder = ImmutableMap.<String, String>builder();
-		if (jsonObject.has("detectCullableFaces")) {
-			automaticCulling = GsonHelper.getAsBoolean(jsonObject, "detectCullableFaces");
-			deprecationWarningsBuilder.put("detectCullableFaces", "automatic_culling");
-		}
-		if (jsonObject.has("diffuseLighting")) {
-			shadeQuads = GsonHelper.getAsBoolean(jsonObject, "diffuseLighting");
-			deprecationWarningsBuilder.put("diffuseLighting", "shade_quads");
-		}
-		if (jsonObject.has("flip-v")) {
-			flipV = GsonHelper.getAsBoolean(jsonObject, "flip-v");
-			deprecationWarningsBuilder.put("flip-v", "flip_v");
-		}
-		if (jsonObject.has("ambientToFullbright")) {
-			emissiveAmbient = GsonHelper.getAsBoolean(jsonObject, "ambientToFullbright");
-			deprecationWarningsBuilder.put("ambientToFullbright", "emissive_ambient");
-		}
-		if (jsonObject.has("materialLibraryOverride")) {
-			mtlOverride = GsonHelper.getAsString(jsonObject, "materialLibraryOverride");
-			deprecationWarningsBuilder.put("materialLibraryOverride", "mtl_override");
-		}
-
-		return loadModel(new ObjModel.ModelSettings(new ResourceLocation(modelLocation), automaticCulling, shadeQuads, flipV, emissiveAmbient, mtlOverride), deprecationWarningsBuilder.build());
-	}
-
-	public ObjModel loadModel(ObjModel.ModelSettings settings) {
-		return loadModel(settings, Map.of());
-	}
-
-	public ObjModel loadModel(Resource resource, ObjModel.ModelSettings settings) {
-		return loadModel(resource, settings, Map.of());
-	}
-
-	private ObjModel loadModel(ObjModel.ModelSettings settings, Map<String, String> deprecationWarnings) {
-		return modelCache.computeIfAbsent(settings, (data) -> {
-			Resource resource = manager.getResource(settings.modelLocation()).orElseThrow();
-			try (ObjTokenizer tokenizer = new ObjTokenizer(resource.open())) {
-				return ObjModel.parse(tokenizer, settings, deprecationWarnings);
-			} catch (FileNotFoundException e) {
-				throw new RuntimeException("Could not find OBJ model", e);
-			} catch (Exception e) {
-				throw new RuntimeException("Could not read OBJ model", e);
+	/**
+	 * models/misc is automatically scanned for OBJ models.
+	 */
+	private void findModels(Consumer<ResourceLocation> out) {
+		manager.listResources("models/misc", id -> {
+			if (id.getPath().endsWith(".json")) {
+				manager.getResource(id).ifPresent(resource -> {
+					if (tryLoadModelJson(id, resource) != null)
+						out.accept(id);
+				});
 			}
+			return true;
 		});
 	}
 
-	private ObjModel loadModel(Resource resource, ObjModel.ModelSettings settings, Map<String, String> deprecationWarnings) {
-		return modelCache.computeIfAbsent(settings, (data) -> {
+	private JsonObject tryLoadModelJson(ResourceLocation id, Resource resource) {
+		try {
+			JsonObject json = JsonParser.parseReader(resource.openAsReader()).getAsJsonObject();
+			if (json.has(OBJ_MARKER)) {
+				return json;
+			}
+		} catch (IOException | IllegalStateException e) {
+			PortingLib.LOGGER.error("Error loading obj model from models/misc: " + id, e);
+		}
+		return null;
+	}
+
+	private Either<ModelSettings, RuntimeException> tryReadSettings(JsonObject json) {
+		try {
+			ResourceLocation objLocation = new ResourceLocation(GsonHelper.getAsString(json, "model"));
+			return Either.left(new ModelSettings(objLocation,
+					GsonHelper.getAsBoolean(json, "automaticCulling", true),
+					GsonHelper.getAsBoolean(json, "shadeQuads", true),
+					GsonHelper.getAsBoolean(json, "flipV", true),
+					GsonHelper.getAsBoolean(json, "emissiveAmbient", true),
+					GsonHelper.getAsString(json, "mtlOverride", null)
+			));
+		} catch (RuntimeException e) { // ID parse fail, json parse fail
+			return Either.right(e);
+		}
+	}
+
+	/**
+	 * Load an OBJ model as a block model's geometry.
+	 */
+	@Override
+	public ObjModel read(JsonObject jsonObject, JsonDeserializationContext deserializationContext) {
+		return tryReadSettings(jsonObject).map(this::loadModel, exception -> {
+			throw new JsonParseException("Error loading OBJ model settings", exception);
+		});
+	}
+
+	private ObjModel loadModel(ObjModel.ModelSettings settings) {
+		return loadModel(manager.getResource(settings.modelLocation()).orElseThrow(), settings);
+	}
+
+	private ObjModel loadModel(Resource resource, ObjModel.ModelSettings settings) {
+		return modelCache.computeIfAbsent(settings, data -> {
 			try (ObjTokenizer tokenizer = new ObjTokenizer(resource.open())) {
-				return ObjModel.parse(tokenizer, settings, deprecationWarnings);
+				return ObjModel.parse(tokenizer, settings);
 			} catch (FileNotFoundException e) {
 				throw new RuntimeException("Could not find OBJ model", e);
 			} catch (Exception e) {
@@ -123,12 +129,8 @@ public class ObjLoader implements IGeometryLoader<ObjModel>, ResourceManagerRelo
 	}
 
 	public ObjMaterialLibrary loadMaterialLibrary(ResourceLocation materialLocation) {
-		return loadMaterialLibrary(materialLocation, null);
-	}
-
-	public ObjMaterialLibrary loadMaterialLibrary(ResourceLocation materialLocation, @Nullable ResourceManager resourceManager) {
-		return materialCache.computeIfAbsent(materialLocation, (location) -> {
-			Resource resource = resourceManager != null ? resourceManager.getResource(location).orElseThrow() : manager.getResource(location).orElseThrow();
+		return materialCache.computeIfAbsent(materialLocation, location -> {
+			Resource resource = manager.getResource(location).orElseThrow();
 			try (ObjTokenizer rdr = new ObjTokenizer(resource.open())) {
 				return new ObjMaterialLibrary(rdr);
 			} catch (FileNotFoundException e) {
@@ -139,13 +141,19 @@ public class ObjLoader implements IGeometryLoader<ObjModel>, ResourceManagerRelo
 		});
 	}
 
-	@Override
-	public ResourceLocation getFabricId() {
-		return PortingLib.id("obj-loader");
-	}
-
-	@Override
-	public Collection<ResourceLocation> getFabricDependencies() {
-		return IdentifiableResourceReloadListener.super.getFabricDependencies();
+	private class Resolver implements ModelResolver {
+		@Override
+		@Nullable
+		public UnbakedModel resolveModel(Context context) {
+			ResourceLocation id = context.id();
+			ResourceLocation fileId = ModelBakery.MODEL_LISTER.idToFile(id);
+			return manager.getResource(fileId).map(resource -> {
+				JsonObject json = tryLoadModelJson(id, resource);
+				return tryReadSettings(json).map(settings -> loadModel(resource, settings), exception -> {
+					PortingLib.LOGGER.error("Error loading obj model: " + id, exception);
+					return null;
+				});
+			}).orElse(null);
+		}
 	}
 }
