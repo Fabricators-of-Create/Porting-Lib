f020c55efbf24270dca387f793d7c0657397bed9
TropheusJ <jverrellijr@icloud.com>
2023-07-19 23:08:47 -0400

    migrate models to model loading v1

--- models/src/main/java/io/github/fabricators_of_create/porting_lib/models/PortingLibModelLoadingRegistry.java
+++ models/src/main/java/io/github/fabricators_of_create/porting_lib/models/PortingLibModelLoadingRegistry.java
@@ -1,24 +1,14 @@
 package io.github.fabricators_of_create.porting_lib.models;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonIOException;
 import com.google.gson.JsonObject;
 
-import com.google.gson.JsonSyntaxException;
-
-import net.fabricmc.fabric.api.client.model.ModelProviderContext;
-import net.fabricmc.fabric.api.client.model.ModelProviderException;
-import net.fabricmc.fabric.api.client.model.ModelResourceProvider;
+import io.github.fabricators_of_create.porting_lib.core.PortingLib;
+import net.fabricmc.fabric.api.client.model.loading.v1.ModelLoadingPlugin;
+import net.fabricmc.fabric.api.client.model.loading.v1.ModelResolver;
 import net.fabricmc.fabric.api.renderer.v1.material.RenderMaterial;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.block.model.BlockElement;
-import net.minecraft.client.renderer.block.model.BlockElementFace;
-import net.minecraft.client.renderer.block.model.BlockFaceUV;
 import net.minecraft.client.renderer.block.model.BlockModel;
-import net.minecraft.client.renderer.block.model.ItemOverride;
-import net.minecraft.client.renderer.block.model.ItemTransform;
-import net.minecraft.client.renderer.block.model.ItemTransforms;
 import net.minecraft.client.resources.model.UnbakedModel;
 import net.minecraft.resources.ResourceLocation;
 
@@ -29,12 +19,13 @@ import net.minecraft.util.GsonHelper;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
-public enum PortingLibModelLoadingRegistry implements ModelResourceProvider {
+public enum PortingLibModelLoadingRegistry implements ModelLoadingPlugin {
 	INSTANCE;
 
 	public static final Map<ResourceLocation, ModelLoader> LOADERS = new HashMap<>();
@@ -42,31 +33,42 @@ public enum PortingLibModelLoadingRegistry implements ModelResourceProvider {
 	public static final Gson GSON = BlockModel.GSON.newBuilder().registerTypeAdapter(RenderMaterial.class, new RenderMaterialDeserializer()).create();
 
 	@Override
-	public @Nullable UnbakedModel loadModelResource(ResourceLocation resourceId, ModelProviderContext context) {
-		try {
-			JsonObject object = GSON.fromJson(getModelJson(resourceId), JsonObject.class);
-			if (object.has("loader")) {
-				ResourceLocation id = new ResourceLocation(GsonHelper.getAsString(object, "loader"));
-				if (!LOADERS.containsKey(id))
-					return null;
-				BlockModel ownerModel = BlockModel.fromString(object.toString());
-				return LOADERS.get(id).readModel(ownerModel, object);
-			}
-		} catch (Exception exception) {
-			return null;
-		}
-
-		return null;
+	public void onInitializeModelLoader(Context ctx) {
+		ctx.resolveModel().register(Resolver.INSTANCE);
 	}
 
-	public static BufferedReader getModelJson(ResourceLocation location) {
+	/**
+	 * Get a reader for the model associated with the provided ID.
+	 * The provided ID is converted from model format (minecraft:block/stone) to file format (minecraft:models/block/stone.json)
+	 * @throws IOException on an IO error
+	 * @throws FileNotFoundException when the model's resource does not exist
+	 */
+	public static BufferedReader getModelJson(ResourceLocation location) throws IOException {
 		ResourceLocation file = new ResourceLocation(location.getNamespace(), "models/" + location.getPath() + ".json");
 		Optional<Resource> resource = Minecraft.getInstance().getResourceManager().getResource(file);
-		try {
-			return resource.get().openAsReader();
-		} catch (IOException e) {
-			e.printStackTrace();
+		return resource.orElseThrow(() -> new FileNotFoundException(file.toString())).openAsReader();
+	}
+
+	private enum Resolver implements ModelResolver {
+		INSTANCE;
+
+		@Override
+		@Nullable
+		public UnbakedModel resolveModel(Context context) {
+			ResourceLocation modelId = context.id();
+			try {
+				JsonObject object = GSON.fromJson(getModelJson(modelId), JsonObject.class);
+				if (object.has("loader")) {
+					ResourceLocation id = new ResourceLocation(GsonHelper.getAsString(object, "loader"));
+					if (!LOADERS.containsKey(id))
+						return null;
+					BlockModel ownerModel = BlockModel.fromString(object.toString());
+					return LOADERS.get(id).readModel(ownerModel, object);
+				}
+			} catch (Exception e) {
+				PortingLib.LOGGER.error("Unhandled error loading model: " + modelId, e);
+			}
+			return null;
 		}
-		return null;
 	}
 }
