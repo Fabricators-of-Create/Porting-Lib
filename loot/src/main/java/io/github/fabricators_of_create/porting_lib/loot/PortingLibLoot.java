b6adaa2127482d098b6d22b745fbf4b0e6e166f3
alpha <alphamodeyt@gmail.com>
2023-10-31 19:08:12 -0500

    Fix merge error

--- loot/src/main/java/io/github/fabricators_of_create/porting_lib/loot/PortingLibLoot.java
+++ loot/src/main/java/io/github/fabricators_of_create/porting_lib/loot/PortingLibLoot.java
@@ -36,7 +36,6 @@ public class PortingLibLoot implements ModInitializer {
 		ResourceManagerHelper.get(PackType.SERVER_DATA).registerReloadListener(LootModifierManager.INSTANCE);
 		DEFERRED_GLOBAL_LOOT_MODIFIER_SERIALIZERS.register();
 		Registry.register(BuiltInRegistries.LOOT_CONDITION_TYPE, PortingLib.id("loot_table_id"), LootTableIdCondition.LOOT_TABLE_ID);
-		Registry.register(BuiltInRegistries.LOOT_CONDITION_TYPE, PortingLib.id("can_tool_perform_action"), CanToolPerformAction.LOOT_CONDITION_TYPE);
 
 		LootTableEvents.MODIFY.register(
 				(resources, manager, id, builder, source) -> ((LootTableBuilderExtensions) builder).port_lib$setId(id)
