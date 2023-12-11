d6239713db3529339de953c3a263841affe702fd
alpha <alphamodeyt@gmail.com>
2023-12-10 19:23:40 -0600

    Add javadoc to EntityStruckByLightningEvent

--- entity/src/main/java/io/github/fabricators_of_create/porting_lib/entity/events/EntityStruckByLightningEvent.java
+++ entity/src/main/java/io/github/fabricators_of_create/porting_lib/entity/events/EntityStruckByLightningEvent.java
@@ -3,6 +3,16 @@ package io.github.fabricators_of_create.porting_lib.entity.events;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LightningBolt;
 
+/**
+ * EntityStruckByLightningEvent is fired when an Entity is about to be struck by lightening.<br>
+ * This event is fired whenever an EntityLightningBolt is updated to strike an Entity in
+ * {@link LightningBolt#tick()}.<br>
+ * <br>
+ * {@link #lightning} contains the instance of EntityLightningBolt attempting to strike an entity.<br>
+ * <br>
+ * If this event is canceled, the Entity is not struck by the lightening.<br>
+ * <br>
+ **/
 public class EntityStruckByLightningEvent extends EntityEvents {
 	private final LightningBolt lightning;
 
