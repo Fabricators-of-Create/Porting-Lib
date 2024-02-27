f6b2781c753d2040c4a772aa0044a269aac15a13
alpha <alphamodeyt@gmail.com>
2024-02-26 23:06:19 -0600

    Add PortingLibEntity.getEntitySpawningPacket

--- modules/entity/src/main/java/io/github/fabricators_of_create/porting_lib/entity/PortingLibEntity.java
+++ modules/entity/src/main/java/io/github/fabricators_of_create/porting_lib/entity/PortingLibEntity.java
@@ -1,10 +1,25 @@
 package io.github.fabricators_of_create.porting_lib.entity;
 
+import com.mojang.logging.LogUtils;
+
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
+import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.ClientGamePacketListener;
+import net.minecraft.network.protocol.game.ClientboundAddEntityPacket;
+import net.minecraft.network.protocol.game.ClientboundBundlePacket;
+import net.minecraft.world.entity.Entity;
+
+import org.jetbrains.annotations.ApiStatus;
+import org.slf4j.Logger;
+
+import java.util.List;
 
 public class PortingLibEntity implements ModInitializer {
-
+	public static final Logger LOGGER = LogUtils.getLogger();
 
 	@Override
 	public void onInitialize() {
@@ -19,7 +34,7 @@ public class PortingLibEntity implements ModInitializer {
 			}
 		});
 		ServerEntityEvents.ENTITY_UNLOAD.register((entity, world) -> {
-			if(entity instanceof MultiPartEntity partEntity && partEntity.isMultipartEntity()) {
+			if (entity instanceof MultiPartEntity partEntity && partEntity.isMultipartEntity()) {
 				PartEntity<?>[] parts = partEntity.getParts();
 				if (parts != null) {
 					for (PartEntity<?> part : parts) {
@@ -28,5 +43,21 @@ public class PortingLibEntity implements ModInitializer {
 				}
 			}
 		});
+	}
+
+	public static Packet<?> getEntitySpawningPacket(Entity entity) {
+		return getEntitySpawningPacket(entity, new ClientboundAddEntityPacket(entity));
+	}
+
+	@ApiStatus.Internal
+	public static Packet<ClientGamePacketListener> getEntitySpawningPacket(Entity entity, Packet<ClientGamePacketListener> base) {
+		if (entity instanceof IEntityAdditionalSpawnData extra) {
+			FriendlyByteBuf buf = PacketByteBufs.create();
+			buf.writeVarInt(entity.getId());
+			extra.writeSpawnData(buf);
+			Packet<ClientGamePacketListener> extraPacket = ServerPlayNetworking.createS2CPacket(IEntityAdditionalSpawnData.EXTRA_DATA_PACKET, buf);
+			return new ClientboundBundlePacket(List.of(base, extraPacket));
+		}
+		return base;
 	}
 }
