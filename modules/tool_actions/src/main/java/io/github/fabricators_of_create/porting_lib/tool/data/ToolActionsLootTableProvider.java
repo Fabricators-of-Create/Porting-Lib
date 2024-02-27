8289befe96c928883819f710ec16820537ecba55
alpha <alphamodeyt@gmail.com>
2024-02-27 17:30:07 -0600

    Re gen data (No more error! :D)

--- modules/tool_actions/src/main/java/io/github/fabricators_of_create/porting_lib/tool/data/ToolActionsLootTableProvider.java
+++ modules/tool_actions/src/main/java/io/github/fabricators_of_create/porting_lib/tool/data/ToolActionsLootTableProvider.java
@@ -1,35 +1,37 @@
 package io.github.fabricators_of_create.porting_lib.tool.data;
 
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
-import javax.annotation.Nullable;
-
 import com.google.common.collect.Maps;
+
 import com.google.common.collect.Multimap;
 
-import io.github.fabricators_of_create.porting_lib.tool.ToolAction;
+import com.google.common.collect.Sets;
+
 import io.github.fabricators_of_create.porting_lib.tool.ToolActions;
 import io.github.fabricators_of_create.porting_lib.tool.loot.CanToolPerformAction;
-import io.github.fabricators_of_create.porting_lib.tool.mixin.BuilderAccessor;
-import io.github.fabricators_of_create.porting_lib.tool.mixin.CompositeEntryBaseAccessor;
-import io.github.fabricators_of_create.porting_lib.tool.mixin.CompositeLootItemConditionAccessor;
-import io.github.fabricators_of_create.porting_lib.tool.mixin.InvertedLootItemConditionAccessor;
-import io.github.fabricators_of_create.porting_lib.tool.mixin.LootPoolEntryContainerAccessor;
 import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.minecraft.Util;
 import net.minecraft.advancements.critereon.ItemPredicate;
+import net.minecraft.core.Holder;
 import net.minecraft.data.CachedOutput;
 import net.minecraft.data.DataProvider;
+import net.minecraft.data.PackOutput;
 import net.minecraft.data.loot.LootTableProvider;
 import net.minecraft.data.loot.LootTableSubProvider;
 import net.minecraft.data.loot.packs.VanillaLootTableProvider;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
 import net.minecraft.util.ProblemReporter;
 import net.minecraft.world.RandomSequence;
 import net.minecraft.world.item.Item;
@@ -43,18 +45,31 @@ import net.minecraft.world.level.storage.loot.LootTable;
 import net.minecraft.world.level.storage.loot.ValidationContext;
 import net.minecraft.world.level.storage.loot.entries.AlternativesEntry;
 import net.minecraft.world.level.storage.loot.entries.CompositeEntryBase;
+import net.minecraft.world.level.storage.loot.entries.DynamicLoot;
+import net.minecraft.world.level.storage.loot.entries.EmptyLootItem;
+import net.minecraft.world.level.storage.loot.entries.EntryGroup;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
 import net.minecraft.world.level.storage.loot.entries.LootPoolEntryContainer;
+import net.minecraft.world.level.storage.loot.entries.LootPoolSingletonContainer;
+import net.minecraft.world.level.storage.loot.entries.LootTableReference;
+import net.minecraft.world.level.storage.loot.entries.SequentialEntry;
+import net.minecraft.world.level.storage.loot.entries.TagEntry;
+import net.minecraft.world.level.storage.loot.functions.LootItemFunction;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.minecraft.world.level.storage.loot.predicates.AllOfCondition;
+import net.minecraft.world.level.storage.loot.predicates.AnyOfCondition;
 import net.minecraft.world.level.storage.loot.predicates.CompositeLootItemCondition;
 import net.minecraft.world.level.storage.loot.predicates.InvertedLootItemCondition;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import net.minecraft.world.level.storage.loot.predicates.MatchTool;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * Currently used only for replacing shears item to shears_dig tool action
  */
 public final class ToolActionsLootTableProvider extends LootTableProvider {
+	private final List<Function<LootItemCondition, LootItemCondition.Builder>> conditionReplacers = new ArrayList<>();
+
 	public ToolActionsLootTableProvider(FabricDataOutput packOutput) {
 		super(packOutput, Set.of(), VanillaLootTableProvider.create(packOutput).subProviders);
 	}
@@ -64,6 +79,12 @@ public final class ToolActionsLootTableProvider extends LootTableProvider {
 	}
 
 	public List<LootTableProvider.SubProviderEntry> getTables() {
+		replaceLootItemCondition(condition -> {
+			if (condition instanceof MatchTool matchTool && checkMatchTool(matchTool, Items.SHEARS)) {
+				return CanToolPerformAction.canToolPerformAction(ToolActions.SHEARS_DIG);
+			}
+			return null;
+		});
 		return this.subProviders.stream().map(entry -> {
 			// Provides new sub provider with filtering only changed loot tables and replacing condition item to condition tag
 			return new LootTableProvider.SubProviderEntry(() -> replaceAndFilterChangesOnly(entry.provider().get()), entry.paramSet());
@@ -72,126 +93,197 @@ public final class ToolActionsLootTableProvider extends LootTableProvider {
 
 	private LootTableSubProvider replaceAndFilterChangesOnly(LootTableSubProvider subProvider) {
 		return newConsumer -> subProvider.generate((resourceLocation, builder) -> {
-			if (findAndReplaceInLootTableBuilder(builder, Items.SHEARS, ToolActions.SHEARS_DIG)) {
-				newConsumer.accept(resourceLocation, builder);
+			LootTable.Builder newBuilder = findAndReplaceInLootTableBuilder(builder);
+			if (newBuilder != null) {
+				newConsumer.accept(resourceLocation, newBuilder);
 			}
 		});
 	}
 
-	private boolean findAndReplaceInLootTableBuilder(LootTable.Builder builder, Item from, ToolAction toolAction) {
-		List<LootPool> lootPools = ((BuilderAccessor) builder).getPools().build();
+	private void replaceLootItemCondition(Function<LootItemCondition, LootItemCondition.Builder> replacer) {
+		conditionReplacers.add(replacer);
+	}
+
+	@Nullable
+	private LootTable.Builder findAndReplaceInLootTableBuilder(LootTable.Builder builder) {
+		LootTable lootTable = builder.build();
+
+		Optional<ResourceLocation> randomSequence = getPrivateValue(LootTable.class, lootTable, "randomSequence");
+		List<LootPool> lootPools = getPrivateValue(LootTable.class, lootTable, "pools");
+		List<LootItemFunction> lootItemFunctions = getPrivateValue(LootTable.class, lootTable, "functions");
+
 		boolean found = false;
+		LootTable.Builder newBuilder = new LootTable.Builder();
+		newBuilder.setParamSet(lootTable.getParamSet());
+		randomSequence.ifPresent(newBuilder::setRandomSequence);
+		for (LootItemFunction lootItemFunction : lootItemFunctions) {
+			newBuilder.apply(() -> lootItemFunction);
+		}
 
 		for (LootPool lootPool : lootPools) {
-			if (findAndReplaceInLootPool(lootPool, from, toolAction)) {
-				found = true;
-			}
+			found |= findAndReplaceInLootPool(lootPool, newBuilder);
 		}
 
-		return found;
+		return found ? newBuilder : null;
 	}
 
-	private boolean findAndReplaceInLootPool(LootPool lootPool, Item from, ToolAction toolAction) {
-		List<LootPoolEntryContainer> lootEntries = lootPool.entries;
-		List<LootItemCondition> lootConditions = lootPool.conditions;
-		boolean found = false;
+	private boolean findAndReplaceInLootPool(LootPool lootPool, LootTable.Builder newBuilder) {
+		List<LootPoolEntryContainer> lootEntries = getPrivateValue(LootPool.class, lootPool, "entries");
+		List<LootItemCondition> lootConditions = getPrivateValue(LootPool.class, lootPool, "conditions");
+		List<LootItemFunction> lootFunctions = getPrivateValue(LootPool.class, lootPool, "functions");
 
-		if (lootEntries == null) {
-			throw new IllegalStateException(LootPool.class.getName() + " is missing field f_7902" + "3_");
-		}
+		LootPool.Builder poolBuilder = new LootPool.Builder();
+		poolBuilder.setRolls(lootPool.rolls);
+		poolBuilder.setBonusRolls(lootPool.bonusRolls);
+//		poolBuilder.name(lootPool.getName());
+		boolean found = false;
 
 		for (LootPoolEntryContainer lootEntry : lootEntries) {
-			if (findAndReplaceInLootEntry(lootEntry, from, toolAction)) {
-				found = true;
-			}
-			if (lootEntry instanceof CompositeEntryBase) {
-				if (findAndReplaceInParentedLootEntry((CompositeEntryBase) lootEntry, from, toolAction)) {
+			found |= findAndReplaceInLootEntry(lootEntry, poolBuilder::add);
+		}
+
+		for (LootItemCondition lootCondition : lootConditions) {
+			if (lootCondition instanceof InvertedLootItemCondition invertedLootItemCondition) {
+				LootItemCondition invLootCondition = invertedLootItemCondition.term();
+
+				Consumer<LootItemCondition.Builder> consumer = cond -> poolBuilder.when(InvertedLootItemCondition.invert(cond));
+				if (invLootCondition instanceof CompositeLootItemCondition compositeLootItemCondition && findAndReplaceInComposite(compositeLootItemCondition, consumer)) {
 					found = true;
+				} else {
+					found |= replaceCondition(invLootCondition, consumer);
 				}
+			} else {
+				found |= replaceCondition(lootCondition, poolBuilder::when);
 			}
 		}
 
-		if (lootConditions == null) {
-			throw new IllegalStateException(LootPool.class.getName() + " is missing field f_7902" + "4_");
+		for (LootItemFunction lootFunction : lootFunctions) {
+			poolBuilder.apply(() -> lootFunction);
 		}
 
-		for (int i = 0; i < lootConditions.size(); i++) {
-			LootItemCondition lootCondition = lootConditions.get(i);
-			if (lootCondition instanceof MatchTool && checkMatchTool((MatchTool) lootCondition, from)) {
-				lootConditions.set(i, CanToolPerformAction.canToolPerformAction(toolAction).build());
-				found = true;
-			} else if (lootCondition instanceof InvertedLootItemCondition) {
-				LootItemCondition invLootCondition = ((InvertedLootItemConditionAccessor) lootCondition).getTerm();
-
-				if (invLootCondition instanceof MatchTool && checkMatchTool((MatchTool) invLootCondition, from)) {
-					lootConditions.set(i, InvertedLootItemCondition.invert(CanToolPerformAction.canToolPerformAction(toolAction)).build());
-					found = true;
-				} else if (invLootCondition instanceof CompositeLootItemCondition compositeLootItemCondition && findAndReplaceInComposite(compositeLootItemCondition, from, toolAction)) {
-					found = true;
-				}
-			}
-		}
+		newBuilder.withPool(poolBuilder);
 
 		return found;
 	}
 
-	private boolean findAndReplaceInParentedLootEntry(CompositeEntryBase entry, Item from, ToolAction toolAction) {
-		List<LootPoolEntryContainer> lootEntries = ((CompositeEntryBaseAccessor) entry).getChildren();
+	private boolean findAndReplaceInParentedLootEntry(CompositeEntryBase entry, Consumer<LootPoolEntryContainer.Builder<?>> newBuilder) {
+		List<LootPoolEntryContainer> lootEntries = getPrivateValue(CompositeEntryBase.class, entry, "children");
+
 		boolean found = false;
-
-		if (lootEntries == null) {
-			throw new IllegalStateException(CompositeEntryBase.class.getName() + " is missing field f_7942" + "8_");
-		}
-
 		for (LootPoolEntryContainer lootEntry : lootEntries) {
-			if (findAndReplaceInLootEntry(lootEntry, from, toolAction)) {
-				found = true;
-			}
+			found |= findAndReplaceInLootEntry(lootEntry, newBuilder);
 		}
 
 		return found;
 	}
 
-	private boolean findAndReplaceInLootEntry(LootPoolEntryContainer entry, Item from, ToolAction toolAction) {
-		List<LootItemCondition> lootConditions = ((LootPoolEntryContainerAccessor) entry).getConditions();
+	private boolean findAndReplaceInLootEntry(LootPoolEntryContainer entry, Consumer<LootPoolEntryContainer.Builder<?>> newBuilder) {
+		List<LootItemCondition> lootConditions = getPrivateValue(LootPoolEntryContainer.class, entry, "conditions");
+
 		boolean found = false;
 
-		if (lootConditions == null) {
-			throw new IllegalStateException(LootPoolEntryContainer.class.getName() + " is missing field f_7963" + "6_");
+		LootPoolEntryContainer.Builder<?> builder;
+		if (entry instanceof CompositeEntryBase compositeEntryBase) {
+			Consumer<LootPoolEntryContainer.Builder<?>> consumer;
+			if (compositeEntryBase instanceof AlternativesEntry) {
+				builder = new AlternativesEntry.Builder();
+				consumer = builder::otherwise;
+			} else if (compositeEntryBase instanceof SequentialEntry) {
+				builder = new SequentialEntry.Builder();
+				consumer = builder::then;
+			} else if (compositeEntryBase instanceof EntryGroup) {
+				builder = new EntryGroup.Builder();
+				consumer = builder::append;
+			} else {
+				throw new IllegalStateException("Unknown CompositeEntryBase type: " + compositeEntryBase.getClass().getName());
+			}
+			found |= findAndReplaceInParentedLootEntry(compositeEntryBase, consumer);
+		} else if (entry instanceof LootPoolSingletonContainer singleton) {
+			if (singleton instanceof DynamicLoot dynamicLoot) {
+				ResourceLocation name = getPrivateValue(DynamicLoot.class, dynamicLoot, "name");
+				builder = DynamicLoot.dynamicEntry(name);
+			} else if (singleton instanceof EmptyLootItem) {
+				builder = EmptyLootItem.emptyItem();
+			} else if (singleton instanceof LootItem lootItem) {
+				Holder<Item> item = getPrivateValue(LootItem.class, lootItem, "item");
+				builder = LootItem.lootTableItem(item.value());
+			} else if (singleton instanceof TagEntry tagEntry) {
+				TagKey<Item> tag = getPrivateValue(TagEntry.class, tagEntry, "tag");
+				boolean expand = getPrivateValue(TagEntry.class, tagEntry, "expand");
+				builder = expand ? TagEntry.expandTag(tag) : TagEntry.tagContents(tag);
+			} else if (singleton instanceof LootTableReference reference) {
+				ResourceLocation name = getPrivateValue(LootTableReference.class, reference, "name");
+				builder = LootTableReference.lootTableReference(name);
+			} else {
+				throw new IllegalStateException("Unknown LootPoolSingletonContainer type: " + singleton.getClass().getName());
+			}
+			int weight = getPrivateValue(LootPoolSingletonContainer.class, singleton, "weight");
+			int quality = getPrivateValue(LootPoolSingletonContainer.class, singleton, "quality");
+			List<LootItemFunction> functions = getPrivateValue(LootPoolSingletonContainer.class, singleton, "functions");
+			((LootPoolSingletonContainer.Builder<?>) builder).setWeight(weight);
+			((LootPoolSingletonContainer.Builder<?>) builder).setQuality(quality);
+			for (LootItemFunction function : functions) {
+				((LootPoolSingletonContainer.Builder<?>) builder).apply(() -> function);
+			}
+		} else {
+			throw new IllegalStateException("Unknown LootPoolEntryContainer type: " + entry.getClass().getName());
 		}
 
-		for (int i = 0; i < lootConditions.size(); i++) {
-			if (lootConditions.get(i) instanceof CompositeLootItemCondition composite && findAndReplaceInComposite(composite, from, toolAction)) {
+		for (LootItemCondition lootCondition : lootConditions) {
+			if (lootCondition instanceof CompositeLootItemCondition composite && findAndReplaceInComposite(composite, builder::when)) {
 				found = true;
-			} else if (lootConditions.get(i) instanceof MatchTool matchTool && checkMatchTool(matchTool, from)) {
-				lootConditions.set(i, CanToolPerformAction.canToolPerformAction(toolAction).build());
-				found = true;
+			} else {
+				found |= replaceCondition(lootCondition, builder::when);
 			}
 		}
+
+		newBuilder.accept(builder);
 
 		return found;
 	}
 
-	private boolean findAndReplaceInComposite(CompositeLootItemCondition alternative, Item from, ToolAction toolAction) {
-		List<LootItemCondition> lootConditions = ((CompositeLootItemConditionAccessor) alternative).getTerms();
+	private boolean findAndReplaceInComposite(CompositeLootItemCondition alternative, Consumer<LootItemCondition.Builder> poolBuilder) {
+		List<LootItemCondition> lootConditions = getPrivateValue(CompositeLootItemCondition.class, alternative, "terms");
+		CompositeLootItemCondition.Builder builder = alternative instanceof AllOfCondition ? new AllOfCondition.Builder() : alternative instanceof AnyOfCondition ? new AnyOfCondition.Builder() : null;
 		boolean found = false;
 
-		if (lootConditions == null) {
-			throw new IllegalStateException(CompositeLootItemCondition.class.getName() + " is missing field f_28560" + "9_");
+		for (LootItemCondition lootCondition : lootConditions) {
+			found |= replaceCondition(lootCondition, builder::addTerm);
 		}
 
-		for (int i = 0; i < lootConditions.size(); i++) {
-			if (lootConditions.get(i) instanceof MatchTool matchTool && checkMatchTool(matchTool, from)) {
-				lootConditions.set(i, CanToolPerformAction.canToolPerformAction(toolAction).build());
-				found = true;
-			}
-		}
+		poolBuilder.accept(builder);
 
 		return found;
 	}
 
 	private boolean checkMatchTool(MatchTool lootCondition, Item expected) {
-		return lootCondition.predicate().flatMap(ItemPredicate::items).map(set -> set.contains(expected.builtInRegistryHolder())).orElse(false);
+		return lootCondition
+				.predicate()
+				.flatMap(ItemPredicate::items)
+				.filter(holders -> holders.contains(expected.builtInRegistryHolder()))
+				.isPresent();
+	}
+
+	private boolean replaceCondition(LootItemCondition lootCondition, Consumer<LootItemCondition.Builder> poolBuilder) {
+		for (Function<LootItemCondition, LootItemCondition.Builder> conditionReplacer : conditionReplacers) {
+			LootItemCondition.Builder newCondition = conditionReplacer.apply(lootCondition);
+			if (newCondition != null) {
+				poolBuilder.accept(newCondition);
+				return true;
+			}
+		}
+		poolBuilder.accept(() -> lootCondition);
+		return false;
+	}
+
+	private <T, C> T getPrivateValue(Class<C> clazz, C inst, String name) {
+		try {
+			var field = clazz.getDeclaredField(name);
+			field.setAccessible(true);
+			return (T) field.get(inst);
+		} catch (IllegalAccessException | NoSuchFieldException e) {
+			throw new IllegalStateException(clazz.getName() + " is missing field " + name);
+		}
 	}
 
 	@Override
@@ -216,6 +308,10 @@ public final class ToolActionsLootTableProvider extends LootTableProvider {
 				return (T) (p_279283_.type() == LootDataType.TABLE ? map.get(p_279283_.location()) : null);
 			}
 		});
+
+		for(ResourceLocation resourceLocation : Sets.difference(this.requiredTables, map.keySet())) {
+			collector.report("Missing built-in table: " + resourceLocation);
+		}
 
 		validate(map, validationcontext);
 
--- modules/tool_actions/src/main/java/io/github/fabricators_of_create/porting_lib/tool/mixin/CompositeLootItemConditionAccessor.java
+++ modules/tool_actions/src/main/java/io/github/fabricators_of_create/porting_lib/tool/mixin/CompositeLootItemConditionAccessor.java
@@ -5,6 +5,7 @@ import net.minecraft.world.level.storage.loot.predicates.CompositeLootItemCondit
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 import java.util.List;
@@ -13,4 +14,8 @@ import java.util.List;
 public interface CompositeLootItemConditionAccessor {
 	@Accessor
 	List<LootItemCondition> getTerms();
+
+	@Mutable
+	@Accessor
+	void setTerms(List<LootItemCondition> terms);
 }
