12dc00fd79d8f817b99c473f0d6d6fdaede518d8
TropheusJ <jverrellijr@icloud.com>
2024-03-01 14:13:22 -0500

    rework StructureTemplate changes

--- modules/core/src/main/java/io/github/fabricators_of_create/porting_lib/core/PortingLib.java
+++ modules/core/src/main/java/io/github/fabricators_of_create/porting_lib/core/PortingLib.java
@@ -1,5 +1,7 @@
 package io.github.fabricators_of_create.porting_lib.core;
 
+import net.fabricmc.loader.api.FabricLoader;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,6 +14,9 @@ public class PortingLib {
 	public static final String ID = "porting_lib";
 	public static final String NAME = "Porting Lib";
 	public static final Logger LOGGER = LoggerFactory.getLogger(NAME);
+	// enables some extra safety checks.
+	public static final boolean STRICT_VALIDATION = FabricLoader.getInstance().isDevelopmentEnvironment()
+			|| Boolean.getBoolean("portingLib.strictValidation");
 
 	public static ResourceLocation id(String path) {
 		return new ResourceLocation(ID, path);
