3f551d12accfce064c34bc0bb8f580accf7bf86a
IThundxr <me@ithundxr.dev>
2024-03-31 15:30:53 -0400

    Switch to @Share

--- base/src/main/java/io/github/fabricators_of_create/porting_lib/mixin/common/MobMixin.java
+++ base/src/main/java/io/github/fabricators_of_create/porting_lib/mixin/common/MobMixin.java
@@ -1,36 +1,33 @@
 package io.github.fabricators_of_create.porting_lib.mixin.common;
 
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-
+import com.llamalad7.mixinextras.sugar.Share;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import io.github.fabricators_of_create.porting_lib.event.common.LivingEntityEvents;
 import io.github.fabricators_of_create.porting_lib.event.common.LivingEntityEvents.ChangeTarget.ChangeTargetEvent;
 import io.github.fabricators_of_create.porting_lib.event.common.LivingEntityEvents.ChangeTarget.ChangeTargetEvent.LivingTargetType;
 import io.github.fabricators_of_create.porting_lib.event.common.MobEntitySetTargetCallback;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Mob.class)
 public abstract class MobMixin {
-	@Unique private ChangeTargetEvent port_lib$changeTargetEvent;
-
 	@Inject(method = "setTarget", at = @At("HEAD"), cancellable = true)
-	private void port_lib$onChangeTarget(LivingEntity target, CallbackInfo ci) {
-		port_lib$changeTargetEvent = new LivingEntityEvents.ChangeTarget.ChangeTargetEvent((Mob) (Object) this, target, LivingTargetType.MOB_TARGET);
-		port_lib$changeTargetEvent.sendEvent();
-		if (port_lib$changeTargetEvent.isCanceled())
+	private void port_lib$onChangeTarget(LivingEntity target, CallbackInfo ci, @Share("changeTargetEvent") LocalRef<ChangeTargetEvent> changeTargetEvent) {
+		changeTargetEvent.set(new LivingEntityEvents.ChangeTarget.ChangeTargetEvent((Mob) (Object) this, target, LivingTargetType.MOB_TARGET));
+		changeTargetEvent.get().sendEvent();
+		if (changeTargetEvent.get().isCanceled())
 			ci.cancel();
 	}
 
 	@WrapOperation(method = "setTarget", at = @At(value = "FIELD", target = "Lnet/minecraft/world/entity/Mob;target:Lnet/minecraft/world/entity/LivingEntity;"))
-	private void port_lib$wrapSetTarget(Mob instance, LivingEntity value, Operation<Void> original) {
-		original.call(instance, port_lib$changeTargetEvent.getNewTarget());
+	private void port_lib$wrapSetTarget(Mob instance, LivingEntity value, Operation<Void> original, @Share("changeTargetEvent") LocalRef<ChangeTargetEvent> changeTargetEvent) {
+		original.call(instance, changeTargetEvent.get().getNewTarget());
 	}
 
 	@Inject(method = "setTarget", at = @At("TAIL"))
--- base/src/main/java/io/github/fabricators_of_create/porting_lib/mixin/common/StartAttackingMixin.java
+++ base/src/main/java/io/github/fabricators_of_create/porting_lib/mixin/common/StartAttackingMixin.java
@@ -1,7 +1,14 @@
 package io.github.fabricators_of_create.porting_lib.mixin.common;
 
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Share;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 
 import io.github.fabricators_of_create.porting_lib.event.common.LivingEntityEvents;
 import io.github.fabricators_of_create.porting_lib.event.common.LivingEntityEvents.ChangeTarget.ChangeTargetEvent;
@@ -11,36 +18,27 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.Brain;
 import net.minecraft.world.entity.ai.behavior.StartAttacking;
-
 import net.minecraft.world.entity.ai.memory.MemoryModuleType;
-
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(StartAttacking.class)
 public class StartAttackingMixin {
-	@Unique private static ChangeTargetEvent port_lib$changeTargetEvent;
-
 	@Inject(method = "setAttackTarget",at = @At("HEAD"), cancellable = true)
-	private static <E extends Mob> void port_lib$onChangeTarget(E mob, LivingEntity attackTarget, CallbackInfo ci) {
-		port_lib$changeTargetEvent = new LivingEntityEvents.ChangeTarget.ChangeTargetEvent(mob, attackTarget, LivingTargetType.BEHAVIOR_TARGET);
-		port_lib$changeTargetEvent.sendEvent();
+	private static <E extends Mob> void port_lib$onChangeTarget(E mob, LivingEntity attackTarget, CallbackInfo ci, @Share("changeTargetEvent") LocalRef<ChangeTargetEvent> changeTargetEvent) {
+		changeTargetEvent.set(new LivingEntityEvents.ChangeTarget.ChangeTargetEvent(mob, attackTarget, LivingTargetType.BEHAVIOR_TARGET));
+		changeTargetEvent.get().sendEvent();
 
-		if (port_lib$changeTargetEvent.isCanceled())
+		if (changeTargetEvent.get().isCanceled())
 			ci.cancel();
 	}
 
 	@WrapOperation(method = "setAttackTarget",at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/ai/Brain;setMemory(Lnet/minecraft/world/entity/ai/memory/MemoryModuleType;Ljava/lang/Object;)V"))
-	private static <U> void port_lib$wrapToChangeTarget(Brain<?> instance, MemoryModuleType<U> memoryType, U memory, Operation<Void> original) {
+	private static <U> void port_lib$wrapToChangeTarget(Brain<?> instance, MemoryModuleType<U> memoryType, U memory, Operation<Void> original, @Share("changeTargetEvent") LocalRef<ChangeTargetEvent> changeTargetEvent) {
 		// Ignore the warning; It's fine; The problem is just that mcdev can't handle generics
-		original.call(instance, memoryType, port_lib$changeTargetEvent.getNewTarget());
+		original.call(instance, memoryType, changeTargetEvent.get().getNewTarget());
 	}
 
 	@Inject(method = "setAttackTarget", at = @At("TAIL"))
-	private static <E extends Mob> void port_lib$onAfterChangeTarget(E attackTarget, LivingEntity owner, CallbackInfo ci) {
-		MobEntitySetTargetCallback.EVENT.invoker().onMobEntitySetTarget(owner, port_lib$changeTargetEvent.getNewTarget());
+	private static <E extends Mob> void port_lib$onAfterChangeTarget(E attackTarget, LivingEntity owner, CallbackInfo ci, @Share("changeTargetEvent") LocalRef<ChangeTargetEvent> changeTargetEvent) {
+		MobEntitySetTargetCallback.EVENT.invoker().onMobEntitySetTarget(owner, changeTargetEvent.get().getNewTarget());
 	}
 }
