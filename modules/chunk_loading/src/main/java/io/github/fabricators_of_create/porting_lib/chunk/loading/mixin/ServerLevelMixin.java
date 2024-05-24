7c2b7ea0a3dd5443228e65801c46e3e3620ee588
Henry Lin <henry.ys.lin@gmail.com>
2024-05-24 21:04:48 +0800

    Fix ServerLevelMixin incorrectly unloading levels with force-loaded chunks

--- modules/chunk_loading/src/main/java/io/github/fabricators_of_create/porting_lib/chunk/loading/mixin/ServerLevelMixin.java
+++ modules/chunk_loading/src/main/java/io/github/fabricators_of_create/porting_lib/chunk/loading/mixin/ServerLevelMixin.java
@@ -5,14 +5,19 @@ import it.unimi.dsi.fastutil.longs.LongSet;
 import net.minecraft.server.level.ServerLevel;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ServerLevel.class)
-public class ServerLevelMixin {
+public abstract class ServerLevelMixin {
+	@Shadow
+	public abstract LongSet getForcedChunks();
+
 	// Future me see if this can be replaced with a modify expression
 	@Redirect(method = "tick", at = @At(value = "INVOKE", target = "Lit/unimi/dsi/fastutil/longs/LongSet;isEmpty()Z"))
 	private boolean hasChunks(LongSet instance) {
-		return PortingLibChunkManager.hasForcedChunks((ServerLevel) (Object) this);
+		//noinspection ConstantValue
+		return getForcedChunks().isEmpty() && !PortingLibChunkManager.hasForcedChunks((ServerLevel) (Object) this);
 	}
 }
