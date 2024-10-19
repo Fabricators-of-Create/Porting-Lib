b976143e6e51e66e2d24297cafb956d9c57ac2fe
Salandora <salandora@gmail.com>
2024-10-19 14:10:20 +0200

    feat(porting): added getFluidContained function

--- modules/transfer/src/main/java/io/github/fabricators_of_create/porting_lib/util/FluidUtil.java
+++ modules/transfer/src/main/java/io/github/fabricators_of_create/porting_lib/util/FluidUtil.java
@@ -1,13 +1,20 @@
 package io.github.fabricators_of_create.porting_lib.util;
 
+import io.github.fabricators_of_create.porting_lib.fluids.FluidStack;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.transfer.v1.context.ContainerItemContext;
+import net.fabricmc.fabric.api.transfer.v1.fluid.FluidStorage;
+import net.fabricmc.fabric.api.transfer.v1.storage.StorageUtil;
 import net.minecraft.Util;
 import net.minecraft.client.resources.language.I18n;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.material.Fluid;
 import net.minecraft.world.level.material.Fluids;
+
+import java.util.Optional;
 
 public class FluidUtil {
 	@Environment(EnvType.CLIENT)
@@ -28,5 +35,22 @@ public class FluidUtil {
 		}
 
 		return translationKey;
+	}
+
+	/**
+	 * Helper method to get the fluid contained in an itemStack
+	 */
+	public static Optional<FluidStack> getFluidContained(ItemStack container) {
+		if (!container.isEmpty()) {
+			container = container.copyWithCount(1);
+			Optional<FluidStack> fluidContained = Optional.ofNullable(ContainerItemContext.withConstant(container).find(FluidStorage.ITEM))
+					.map(handler -> StorageUtil.findExtractableContent(handler, null))
+					.map(FluidStack::new);
+
+			if (fluidContained.isPresent() && !fluidContained.get().isEmpty()) {
+				return fluidContained;
+			}
+		}
+		return Optional.empty();
 	}
 }
