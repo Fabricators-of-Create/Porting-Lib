982f93c24d4f15489cea2106b2e16fc372f3d885
TropheusJ <jverrellijr@icloud.com>
2024-11-26 21:31:29 -0500

    fix compat with FAPI's core shader API fix #110

--- src/main/java/io/github/fabricators_of_create/porting_lib/mixin/client/ShaderInstanceMixin.java
+++ src/main/java/io/github/fabricators_of_create/porting_lib/mixin/client/ShaderInstanceMixin.java
@@ -5,23 +5,20 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
-
-import com.mojang.blaze3d.shaders.Program;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.renderer.ShaderInstance;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.packs.resources.ResourceProvider;
 
 @Environment(EnvType.CLIENT)
-@Mixin(value = ShaderInstance.class, priority = 500)
+@Mixin(value = ShaderInstance.class, priority = 1500) // apply after FAPI so we get the modified ID below. FAPI doesn't have handling for an already fixed one.
 public abstract class ShaderInstanceMixin {
 	@Shadow
 	@Final
 	private String name;
 
+	// we still need to do this despite FAPI's API since it only checks for FabricShader
 	@ModifyArg(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/resources/ResourceLocation;<init>(Ljava/lang/String;)V", ordinal = 0))
 	private String fixId(String path) {
 		if (!path.contains(":")) {
