07bc429baf69ca329c61a0f41ae0e2f5779f4771
Naz <bluspringindohq@gmail.com>
2025-07-26 20:39:54 +0800

    Some additional javadoc for CustomDamageItem

--- modules/items/src/main/java/io/github/fabricators_of_create/porting_lib/item/extensions/CustomDamageItem.java
+++ modules/items/src/main/java/io/github/fabricators_of_create/porting_lib/item/extensions/CustomDamageItem.java
@@ -1,5 +1,7 @@
 package io.github.fabricators_of_create.porting_lib.item.extensions;
 
+import net.fabricmc.fabric.api.item.v1.CustomDamageHandler;
+import net.fabricmc.fabric.api.item.v1.FabricItem;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -18,6 +20,7 @@ public interface CustomDamageItem {
 	 * @param entity   The entity damaging the item
 	 * @param onBroken The on-broken callback from vanilla
 	 * @return The amount of damage to pass to the vanilla logic
+	 * @apiNote Try to use {@link FabricItem.Settings#customDamage(CustomDamageHandler)} instead where possible.
 	 */
 	default <T extends LivingEntity> int damageItem(ItemStack stack, int amount, @Nullable T entity, Consumer<Item> onBroken) {
 		return amount;
