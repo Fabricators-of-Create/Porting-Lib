12a4d68bfddea71daada211af6547a93dd04f942
alpha <alphamodeyt@gmail.com>
2025-07-26 21:07:46 -0500

    Add TranslatableEnum

---
+++ modules/common/src/main/java/io/github/fabricators_of_create/porting_lib/common/TranslatableEnum.java
@@ -0,0 +1,16 @@
+package io.github.fabricators_of_create.porting_lib.common;
+
+import net.minecraft.network.chat.Component;
+
+/**
+ * An enum value that can be be translated.
+ */
+public interface TranslatableEnum {
+	/**
+	 * {@return the translated name of this value}
+	 * Defaults to a {@linkplain Component#literal(String) literal component} with the {@link Enum#name() enum name};
+	 */
+	default Component getTranslatedName() {
+		return Component.literal(((Enum<?>) this).name());
+	}
+}
