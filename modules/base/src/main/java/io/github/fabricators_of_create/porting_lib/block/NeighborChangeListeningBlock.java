50f7f9a02bc5f03d260ebdf4617070219e10068d
ner24 <manoonia@hotmail.com>
2025-08-10 15:52:00 +0100

    switched to using NeighborChangeListeningBlock (as well as added getWeakChanges to it), updated Comparator block to fix bug where it wont update via the vault

--- modules/base/src/main/java/io/github/fabricators_of_create/porting_lib/block/NeighborChangeListeningBlock.java
+++ modules/base/src/main/java/io/github/fabricators_of_create/porting_lib/block/NeighborChangeListeningBlock.java
@@ -5,5 +5,24 @@ import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.state.BlockState;
 
 public interface NeighborChangeListeningBlock {
+
+	/**
+	 * Used by the neighbouring blocks to notify this block that that neighbour updated
+	 * @param state Block state at position pos
+	 * @param world The world
+	 * @param pos Block position
+	 * @param neighbor Neighbour position
+	 */
 	void onNeighborChange(BlockState state, LevelReader world, BlockPos pos, BlockPos neighbor);
+
+	/**
+	 * Check if this block should be notified of weak changes. <br>
+	 * Weak changes are changes 1 block away through a solid block. Similar to comparators.
+	 * @param level The world
+	 * @param pos Block position
+	 * @return true if there are weak changes to check. False otherwise
+	 */
+	default boolean getWeakChanges(BlockState state, LevelReader level, BlockPos pos) {
+		return false;
+	}
 }
---
+++ modules/base/src/main/java/io/github/fabricators_of_create/porting_lib/extensions/ComparatorBlockExtension.java
@@ -0,0 +1,6 @@
+package io.github.fabricators_of_create.porting_lib.extensions;
+
+import io.github.fabricators_of_create.porting_lib.block.NeighborChangeListeningBlock;
+
+public interface ComparatorBlockExtension extends NeighborChangeListeningBlock {
+}
