6b7e54562033f39c58a999983bfd32c437d73b3f
Naz <bluspringindohq@gmail.com>
2025-08-28 00:16:32 +0800

    Re-add old callback events with a deprecation warning

---
+++ modules/base/src/main/java/io/github/fabricators_of_create/porting_lib/mixin/client/SkullBlockRendererMixin.java
@@ -0,0 +1,27 @@
+package io.github.fabricators_of_create.porting_lib.mixin.client;
+
+import com.google.common.collect.ImmutableMap;
+
+import com.llamalad7.mixinextras.sugar.Local;
+
+import io.github.fabricators_of_create.porting_lib.event.client.CreateSkullModelsCallback;
+import net.minecraft.client.model.SkullModelBase;
+import net.minecraft.client.model.geom.EntityModelSet;
+import net.minecraft.client.renderer.blockentity.SkullBlockRenderer;
+import net.minecraft.world.level.block.SkullBlock;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import java.util.Map;
+
+@Mixin(SkullBlockRenderer.class)
+public class SkullBlockRendererMixin {
+	@Inject(method = "createSkullRenderers", at = @At(value = "INVOKE", target = "Lcom/google/common/collect/ImmutableMap$Builder;put(Ljava/lang/Object;Ljava/lang/Object;)Lcom/google/common/collect/ImmutableMap$Builder;", ordinal = 5))
+	private static void port_lib$createSkullModels(EntityModelSet entityModelSet, CallbackInfoReturnable<Map<SkullBlock.Type, SkullModelBase>> cir, @Local ImmutableMap.Builder<SkullBlock.Type, SkullModelBase> builder) {
+		CreateSkullModelsCallback.EVENT.invoker().onSkullModelsCreated(builder, entityModelSet);
+	}
+}
