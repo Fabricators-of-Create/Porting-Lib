52ab7db475268e49cb2dd579daf74712c060ba56
alpha <alphamodeyt@gmail.com>
2025-09-11 21:51:05 -0500

    fix recipe_book_categories module

--- modules/recipe_book_categories/src/client/java/io/github/fabricators_of_create/porting_lib/recipe_book_categories/mixin/ClientRecipeBookMixin.java
+++ modules/recipe_book_categories/src/client/java/io/github/fabricators_of_create/porting_lib/recipe_book_categories/mixin/ClientRecipeBookMixin.java
@@ -20,7 +20,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.util.List;
 import java.util.Map;
