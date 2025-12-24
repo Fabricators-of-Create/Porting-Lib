7ba3e4fb5027834f65275a918a46545a9bf25d8c
Naz <bluspringindohq@gmail.com>
2025-12-24 20:51:07 +0800

    Revert "Add some workarounds to make ItemStack/MutableDataComponentHolder work in production"
    This reverts commit 0b61a42aefc4e6d9c20bafba0f8f3fff41b0ba0a.

--- modules/core/src/main/java/io/github/fabricators_of_create/porting_lib/core/util/MutableDataComponentHolder.java
+++ modules/core/src/main/java/io/github/fabricators_of_create/porting_lib/core/util/MutableDataComponentHolder.java
@@ -7,7 +7,6 @@ import net.minecraft.core.component.DataComponentHolder;
 import net.minecraft.core.component.DataComponentMap;
 import net.minecraft.core.component.DataComponentPatch;
 import net.minecraft.core.component.DataComponentType;
-import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.Nullable;
 
 public interface MutableDataComponentHolder extends DataComponentHolder {
@@ -15,19 +14,7 @@ public interface MutableDataComponentHolder extends DataComponentHolder {
 	 * Sets a data component.
 	 */
 	@Nullable
-	default <T> T set(DataComponentType<? super T> componentType, @Nullable T value) {
-		return this.method_57379(componentType, value);
-	}
-
-	/**
-	 * Just like {@link MutableDataComponentHolder#set(Supplier, Object)}, except this runs in production environments.
-	 */
-	@Deprecated(forRemoval = true)
-	@ApiStatus.NonExtendable
-	@Nullable
-	default <T> T method_57379(DataComponentType<? super T> componentType, @Nullable T value) {
-		return this.set(componentType, value);
-	}
+	<T> T set(DataComponentType<? super T> componentType, @Nullable T value);
 
 	/**
 	 * Sets a data component.
@@ -73,19 +60,7 @@ public interface MutableDataComponentHolder extends DataComponentHolder {
 	 * Removes a data component.
 	 */
 	@Nullable
-	default <T> T remove(DataComponentType<? extends T> componentType) {
-		return this.method_57381(componentType);
-	}
-
-	/**
-	 * Just like {@link MutableDataComponentHolder#remove(DataComponentType)}, except this runs in production environments.
-	 */
-	@Deprecated(forRemoval = true)
-	@ApiStatus.NonExtendable
-	@Nullable
-	default <T> T method_57381(DataComponentType<? extends T> componentType) {
-		return this.remove(componentType);
-	}
+	<T> T remove(DataComponentType<? extends T> componentType);
 
 	/**
 	 * Removes a data component.
@@ -120,34 +95,12 @@ public interface MutableDataComponentHolder extends DataComponentHolder {
 	/**
 	 * Applies a set of component changes to this stack.
 	 */
-	default void applyComponents(DataComponentPatch patch) {
-		this.method_57366(patch);
-	}
-
-	/**
-	 * Just like {@link MutableDataComponentHolder#applyComponents(DataComponentPatch)}, except this runs in production environments.
-	 */
-	@Deprecated(forRemoval = true)
-	@ApiStatus.NonExtendable
-	default void method_57366(DataComponentPatch patch) {
-		this.applyComponents(patch);
-	}
+	void applyComponents(DataComponentPatch patch);
 
 	/**
 	 * Applies a set of component changes to this stack.
 	 */
-	default void applyComponents(DataComponentMap components) {
-		this.method_57365(components);
-	}
-
-	/**
-	 * Just like {@link MutableDataComponentHolder#applyComponents(DataComponentMap)}, except this runs in production environments.
-	 */
-	@Deprecated(forRemoval = true)
-	@ApiStatus.NonExtendable
-	default void method_57365(DataComponentMap patch) {
-		this.applyComponents(patch);
-	}
+	void applyComponents(DataComponentMap components);
 
 	private <T> void copyFrom(DataComponentType<T> componentType, DataComponentHolder src) {
 		set(componentType, src.get(componentType));
