6a4b1b2a51ac0b6e71e47646535bdb657b4e4e14
alpha <alphamodeyt@gmail.com>
2025-12-24 19:17:44 -0600

    Update PortingLibCreativeTab

--- modules/items/src/client/java/io/github/fabricators_of_create/porting_lib/item/client/mixin/CreativeModeInventoryScreenMixin.java
+++ modules/items/src/client/java/io/github/fabricators_of_create/porting_lib/item/client/mixin/CreativeModeInventoryScreenMixin.java
@@ -1,27 +1,85 @@
 package io.github.fabricators_of_create.porting_lib.item.client.mixin;
 
-import io.github.fabricators_of_create.porting_lib.item.extensions.CreativeModeTabExt;
-import io.github.fabricators_of_create.porting_lib.item.itemgroup.PortingLibCreativeTab;
-import net.minecraft.client.gui.screens.inventory.CreativeModeInventoryScreen;
+import net.fabricmc.fabric.api.client.itemgroup.v1.FabricCreativeInventoryScreen;
+import net.minecraft.client.gui.components.EditBox;
 
-import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.Identifier;
+import net.minecraft.world.entity.player.Inventory;
+
+import net.minecraft.world.inventory.AbstractContainerMenu;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
-@Mixin(CreativeModeInventoryScreen.class)
-public class CreativeModeInventoryScreenMixin {
-	@Shadow
-	private static CreativeModeTab selectedTab;
+import com.llamalad7.mixinextras.expression.Definition;
+import com.llamalad7.mixinextras.expression.Expression;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 
-	@ModifyArg(method = "renderLabels", index = 4, at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;drawString(Lnet/minecraft/client/gui/Font;Lnet/minecraft/network/chat/Component;IIIZ)I"))
+import io.github.fabricators_of_create.porting_lib.item.extensions.CreativeModeTabExt;
+import io.github.fabricators_of_create.porting_lib.item.itemgroup.PortingLibCreativeTab;
+import net.minecraft.client.gui.screens.inventory.CreativeModeInventoryScreen;
+import net.minecraft.world.item.CreativeModeTab;
+
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(CreativeModeInventoryScreen.class)
+public abstract class CreativeModeInventoryScreenMixin extends AbstractContainerScreen implements FabricCreativeInventoryScreen {
+	@Shadow
+	private EditBox searchBox;
+
+	public CreativeModeInventoryScreenMixin(AbstractContainerMenu menu, Inventory playerInventory, Component title) {
+		super(menu, playerInventory, title);
+	}
+
+	@ModifyArg(method = "renderLabels", index = 4, at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;drawString(Lnet/minecraft/client/gui/Font;Lnet/minecraft/network/chat/Component;IIIZ)V"))
 	private int modifyLabelColor(int labelColor) {
-		PortingLibCreativeTab.TabData data = ((CreativeModeTabExt) selectedTab).getPortingTabData();
+		PortingLibCreativeTab.TabData data = ((CreativeModeTabExt) getSelectedItemGroup()).getPortingTabData();
 		if (data != null)
 			return data.labelColor();
 		return labelColor;
+	}
+
+	@ModifyArg(method = "renderBg", index = 1, at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;blitSprite(Lcom/mojang/blaze3d/pipeline/RenderPipeline;Lnet/minecraft/resources/Identifier;IIII)V"))
+	private Identifier modifyScrollerSprite(Identifier identifier) {
+		PortingLibCreativeTab.TabData data = ((CreativeModeTabExt) getSelectedItemGroup()).getPortingTabData();
+		if (data != null)
+			return data.scrollerSprite();
+		return identifier;
+	}
+
+	@Definition(id = "SEARCH", field = "Lnet/minecraft/world/item/CreativeModeTab$Type;SEARCH:Lnet/minecraft/world/item/CreativeModeTab$Type;")
+	@Expression("? == SEARCH")
+	@ModifyExpressionValue(method = {"refreshCurrentTabContents", "selectTab", "getTooltipFromContainerItem"}, at = @At("MIXINEXTRAS:EXPRESSION"))
+	private boolean hasSearchBar(boolean original) {
+		PortingLibCreativeTab.TabData data = ((CreativeModeTabExt) getSelectedItemGroup()).getPortingTabData();
+		if (data != null)
+			return data.hasSearchBar();
+		return original;
+	}
+
+	@Definition(id = "SEARCH", field = "Lnet/minecraft/world/item/CreativeModeTab$Type;SEARCH:Lnet/minecraft/world/item/CreativeModeTab$Type;")
+	@Expression("? != SEARCH")
+	@ModifyExpressionValue(method = {"getTooltipFromContainerItem", "charTyped", "keyPressed"}, at = @At("MIXINEXTRAS:EXPRESSION"))
+	private boolean doesntHaveSearchBar(boolean original) {
+		PortingLibCreativeTab.TabData data = ((CreativeModeTabExt) getSelectedItemGroup()).getPortingTabData();
+		if (data != null)
+			return data.hasSearchBar();
+		return original;
+	}
+
+	@Inject(method = "selectTab", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/inventory/CreativeModeInventoryScreen;refreshSearchResults()V"))
+	private void setSearchBarWidth(CreativeModeTab tab, CallbackInfo ci) {
+		PortingLibCreativeTab.TabData data = ((CreativeModeTabExt) getSelectedItemGroup()).getPortingTabData();
+		if (data != null) {
+			this.searchBox.setWidth(data.searchBarWidth());
+			this.searchBox.setX(this.leftPos + (82 /*default left*/ + 89 /*default width*/) - this.searchBox.getWidth());
+		}
 	}
 
 //	@ModifyArg(method = "renderBg", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;blit(Lnet/minecraft/resources/ResourceLocation;IIIIII)V", ordinal = 1))
--- modules/items/src/main/java/io/github/fabricators_of_create/porting_lib/item/itemgroup/PortingLibCreativeTab.java
+++ modules/items/src/main/java/io/github/fabricators_of_create/porting_lib/item/itemgroup/PortingLibCreativeTab.java
@@ -1,29 +1,48 @@
 package io.github.fabricators_of_create.porting_lib.item.itemgroup;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 import io.github.fabricators_of_create.porting_lib.item.extensions.CreativeModeTabExt;
+import net.minecraft.core.Holder;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.flag.FeatureFlagSet;
 import net.minecraft.world.item.CreativeModeTab;
 
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.ItemLike;
+
+import org.jspecify.annotations.Nullable;
+
 public class PortingLibCreativeTab {
+	private static final Identifier SCROLLER_SPRITE = Identifier.withDefaultNamespace("container/creative_inventory/scroller");
+	private static final Identifier SCROLLER_DISABLED_SPRITE = Identifier.withDefaultNamespace("container/creative_inventory/scroller_disabled");
+
 	public static PortingLibCreativeTabBuilder builder() {
 		return new PortingLibCreativeTabBuilder();
 	}
 
 	public static class PortingLibCreativeTabBuilder extends CreativeModeTab.Builder {
-		public static final ResourceLocation CREATIVE_TABS_LOCATION = ResourceLocation.withDefaultNamespace("textures/gui/container/creative_inventory/tabs.png");
+		private static final net.minecraft.resources.Identifier CREATIVE_INVENTORY_TABS_IMAGE = net.minecraft.resources.Identifier.withDefaultNamespace("textures/gui/container/creative_inventory/tabs.png");
+		private static final net.minecraft.resources.Identifier CREATIVE_ITEM_SEARCH_BACKGROUND = CreativeModeTab.createTextureLocation("item_search");
 
-		private boolean hasDisplayName = false;
-		private ResourceLocation tabsImage = CREATIVE_TABS_LOCATION;
-		private int labelColor = 4210752;
+		private @Nullable Identifier spriteScrollerLocation;
+		private boolean hasSearchBar = false;
+		private int searchBarWidth = 89;
+		private Identifier tabsImage = CREATIVE_INVENTORY_TABS_IMAGE;
+		private int labelColor = -12566464;
 		private int slotColor = -2130706433;
-		private final List<ResourceLocation> tabsBefore = new ArrayList<>();
-		private final List<ResourceLocation> tabsAfter = new ArrayList<>();
+		private Function<PortingLibCreativeTabBuilder, CreativeModeTab> tabFactory = null;
+		private final List<Identifier> tabsBefore = new ArrayList<>();
+		private final List<Identifier> tabsAfter = new ArrayList<>();
 
 		public PortingLibCreativeTabBuilder() {
 			// Set when building.
@@ -31,26 +50,99 @@ public class PortingLibCreativeTab {
 		}
 
 		@Override
-		public PortingLibCreativeTabBuilder title(Component displayName) {
-			hasDisplayName = true;
-			return (PortingLibCreativeTabBuilder) super.title(displayName);
+		public PortingLibCreativeTabBuilder title(Component title) {
+			super.title(title);
+			return this;
+		}
+
+		@Override
+		public PortingLibCreativeTabBuilder icon(Supplier<ItemStack> icon) {
+			super.icon(icon);
+			return this;
+		}
+
+		@Override
+		public PortingLibCreativeTabBuilder displayItems(CreativeModeTab.DisplayItemsGenerator displayItemsGenerator) {
+			super.displayItems(displayItemsGenerator);
+			return this;
+		}
+
+		@Override
+		public PortingLibCreativeTabBuilder alignedRight() {
+			super.alignedRight();
+			return this;
+		}
+
+		@Override
+		public PortingLibCreativeTabBuilder hideTitle() {
+			super.hideTitle();
+			return this;
+		}
+
+		@Override
+		public PortingLibCreativeTabBuilder noScrollBar() {
+			super.noScrollBar();
+			return this;
+		}
+
+		@Override
+		public PortingLibCreativeTabBuilder backgroundTexture(Identifier identifier) {
+			super.backgroundTexture(identifier);
+			return this;
 		}
 
 		@Override
 		public CreativeModeTab build() {
-			if (!hasDisplayName) {
-				throw new IllegalStateException("No display name set for ItemGroup");
-			}
-
-			CreativeModeTab tab = super.build();
-			((CreativeModeTabExt) tab).setPortingData(new TabData(tabsImage, labelColor, slotColor, tabsBefore, tabsAfter));
+			CreativeModeTab tab;
+			if (tabFactory != null)
+				tab = tabFactory.apply(this);
+			else
+				tab = super.build();
+			((CreativeModeTabExt) tab).setPortingData(new TabData(hasSearchBar, searchBarWidth, tabsImage, labelColor, slotColor, spriteScrollerLocation, tabsBefore, tabsAfter));
 			return tab;
+		}
+
+		@Override
+		protected PortingLibCreativeTabBuilder type(CreativeModeTab.Type type) {
+			super.type(type);
+			if (type == CreativeModeTab.Type.SEARCH)
+				return this.withSearchBar();
+			return this;
+		}
+
+		/**
+		 * Gives this tab a search bar.
+		 * <p>Note that, if using a custom {@link #backgroundTexture(net.minecraft.resources.Identifier) background image}, you will need to make sure that your image contains the input box and the scroll bar.</p>
+		 */
+		public PortingLibCreativeTabBuilder withSearchBar() {
+			this.hasSearchBar = true;
+			if (this.backgroundTexture == CreativeModeTab.DEFAULT_BACKGROUND)
+				return this.backgroundTexture(CREATIVE_ITEM_SEARCH_BACKGROUND);
+			return this;
+		}
+
+		/**
+		 * Gives this tab a search bar, with a specific width.
+		 * @param searchBarWidth the width of the search bar
+		 */
+		public PortingLibCreativeTabBuilder withSearchBar(int searchBarWidth) {
+			this.searchBarWidth = searchBarWidth;
+			return withSearchBar();
+		}
+
+		/**
+		 * Sets the location of the scroll bar background.
+		 */
+		public PortingLibCreativeTabBuilder withScrollBarSpriteLocation(Identifier scrollBarSpriteLocation) {
+			this.spriteScrollerLocation = scrollBarSpriteLocation;
+			return this;
 		}
 
 		/**
 		 * Sets the image of the tab to a custom resource location, instead of an item's texture.
 		 */
-		public PortingLibCreativeTabBuilder withTabsImage(ResourceLocation tabsImage) {
+		@Deprecated(forRemoval = true) // Currently does nothing for neo and fabric
+		public PortingLibCreativeTabBuilder withTabsImage(Identifier tabsImage) {
 			this.tabsImage = tabsImage;
 			return this;
 		}
@@ -71,32 +163,52 @@ public class PortingLibCreativeTab {
 			return this;
 		}
 
+		public PortingLibCreativeTabBuilder withTabFactory(Function<PortingLibCreativeTabBuilder, CreativeModeTab> tabFactory) {
+			this.tabFactory = tabFactory;
+			return this;
+		}
+
 		/** Define tabs that should come <i>before</i> this tab. This tab will be placed <strong>after</strong> the {@code tabs}. **/
-		public PortingLibCreativeTabBuilder withTabsBefore(ResourceLocation... tabs) {
-			this.tabsBefore.addAll(List.of(tabs));
+		public PortingLibCreativeTabBuilder withTabsBefore(Identifier... tabs) {
+			this.tabsBefore.addAll(java.util.List.of(tabs));
 			return this;
 		}
 
 		/** Define tabs that should come <i>after</i> this tab. This tab will be placed <strong>before</strong> the {@code tabs}.**/
-		public PortingLibCreativeTabBuilder withTabsAfter(ResourceLocation... tabs) {
-			this.tabsAfter.addAll(List.of(tabs));
+		public PortingLibCreativeTabBuilder withTabsAfter(Identifier... tabs) {
+			this.tabsAfter.addAll(java.util.List.of(tabs));
 			return this;
 		}
 
 		/** Define tabs that should come <i>before</i> this tab. This tab will be placed <strong>after</strong> the {@code tabs}. **/
 		@SafeVarargs
 		public final PortingLibCreativeTabBuilder withTabsBefore(ResourceKey<CreativeModeTab>... tabs) {
-			Stream.of(tabs).map(ResourceKey::location).forEach(this.tabsBefore::add);
+			Stream.of(tabs).map(ResourceKey::identifier).forEach(this.tabsBefore::add);
 			return this;
 		}
 
 		/** Define tabs that should come <i>after</i> this tab. This tab will be placed <strong>before</strong> the {@code tabs}.**/
 		@SafeVarargs
 		public final PortingLibCreativeTabBuilder withTabsAfter(ResourceKey<CreativeModeTab>... tabs) {
-			Stream.of(tabs).map(ResourceKey::location).forEach(this.tabsAfter::add);
+			java.util.stream.Stream.of(tabs).map(ResourceKey::identifier).forEach(this.tabsAfter::add);
 			return this;
+		}
+
+		/**
+		 * Helper to set this tabs contents to everything in the supplied Collection of Holders.
+		 * Intended for use with {@link io.github.fabricators_of_create.porting_lib.registry.DeferredRegister#getEntries()}, for Item or Block DeferredRegisters.
+		 * Entries added through this method are filtered out if {@link ItemLike#asItem()} returns {@link Items.AIR} or if disabled via {@link Item#isEnabled(FeatureFlagSet)}.
+		 * Note that like the vanilla method this overrides any other calls for this tab to {@link #displayItems}.
+		 */
+		public PortingLibCreativeTabBuilder displayItems(Collection<? extends Holder<? extends ItemLike>> collection) {
+			return this.displayItems((p, o) -> collection.stream()
+					.map(net.minecraft.core.Holder::value)
+					.map(ItemLike::asItem)
+					.filter(i -> i != Items.AIR)
+					.filter(i -> i.isEnabled(p.enabledFeatures()))
+					.forEach(o::accept));
 		}
 	}
 
-	public record TabData(ResourceLocation tabsImage, int labelColor, int slotColor, List<ResourceLocation> tabsBefore, List<ResourceLocation> tabsAfter) {}
+	public record TabData(boolean hasSearchBar, int searchBarWidth, Identifier tabsImage, int labelColor, int slotColor, Identifier scrollerSprite, List<Identifier> tabsBefore, List<Identifier> tabsAfter) {}
 }
