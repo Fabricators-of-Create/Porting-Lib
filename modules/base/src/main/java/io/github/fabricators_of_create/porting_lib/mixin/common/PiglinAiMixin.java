fa345e85ab8040e051fd2323cd8f787a7ce460c3
alpha <alphamodeyt@gmail.com>
2026-01-08 13:09:13 -0600

    This should actually fix PiglinsNeutralItem

--- modules/base/src/main/java/io/github/fabricators_of_create/porting_lib/mixin/common/PiglinAiMixin.java
+++ modules/base/src/main/java/io/github/fabricators_of_create/porting_lib/mixin/common/PiglinAiMixin.java
@@ -1,5 +1,7 @@
 package io.github.fabricators_of_create.porting_lib.mixin.common;
 
+import com.llamalad7.mixinextras.sugar.Local;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,13 +14,10 @@ import net.minecraft.world.item.ItemStack;
 
 @Mixin(PiglinAi.class)
 public class PiglinAiMixin {
-	@Inject(method = "isWearingGold", at = @At("HEAD"), cancellable = true)
-	private static void port_lib$isNeutralItem(LivingEntity entity, CallbackInfoReturnable<Boolean> cir) {
-		for(ItemStack armor : entity.getArmorSlots()) {
-			if(armor.getItem() instanceof PiglinsNeutralItem piglinsNeutralItem) {
-				cir.setReturnValue(piglinsNeutralItem.makesPiglinsNeutral(armor, entity));
-				return;
-			}
+	@Inject(method = "isWearingGold", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;getItem()Lnet/minecraft/world/item/Item;"), cancellable = true)
+	private static void port_lib$isNeutralItem(LivingEntity entity, CallbackInfoReturnable<Boolean> cir, @Local ItemStack armor) {
+		if(armor.getItem() instanceof PiglinsNeutralItem piglinsNeutralItem) {
+			cir.setReturnValue(piglinsNeutralItem.makesPiglinsNeutral(armor, entity));
 		}
 	}
 }
