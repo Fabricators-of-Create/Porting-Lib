627b9610c5040716eb0ec954c62a232dc0570c40
Niklas Widmann <nik@somethingcatchy.net>
2026-05-13 19:36:32 +0200

    allow ModPathPackResources to return null for a file if not present

--- modules/data/src/main/java/io/github/fabricators_of_create/porting_lib/resources/ModPathPackResources.java
+++ modules/data/src/main/java/io/github/fabricators_of_create/porting_lib/resources/ModPathPackResources.java
@@ -1,23 +1,35 @@
 package io.github.fabricators_of_create.porting_lib.resources;
 
-import net.fabricmc.loader.api.ModContainer;
-
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.NoSuchElementException;
+import net.fabricmc.loader.api.ModContainer;
+import net.minecraft.server.packs.resources.IoSupplier;
+import org.jetbrains.annotations.Nullable;
 
 public class ModPathPackResources extends PathPackResources {
 
-	private final ModContainer mf;
+    private final ModContainer mf;
 
-	public ModPathPackResources(ModContainer mf) {
-		super(mf.getMetadata().getName(), false, mf.getRootPath());
-		this.mf = mf;
-	}
+    public ModPathPackResources(ModContainer mf) {
+        super(mf.getMetadata().getName(), false, mf.getRootPath());
+        this.mf = mf;
+    }
 
-	@Override
-	protected Path resolve(String... paths) {
-		String path = String.join("/", paths);
-		return mf.findPath(path).orElseThrow(() -> new NoSuchElementException("Path " + path + " not found"));
-	}
+    public @Nullable IoSupplier<InputStream> getRootResource(String... paths) {
+        try {
+            Path path = this.resolve(paths);
+            return !Files.exists(path) ? null : IoSupplier.create(path);
+        } catch (NoSuchElementException ex) {
+            return null;
+        }
+    }
+
+    @Override
+    protected Path resolve(String... paths) {
+        String path = String.join("/", paths);
+        return mf.findPath(path).orElseThrow(() -> new NoSuchElementException("Path " + path + " not found"));
+    }
 
 }
