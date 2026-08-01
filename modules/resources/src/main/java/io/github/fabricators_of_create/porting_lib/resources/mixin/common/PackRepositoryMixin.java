7a2baa4ecbab78ed2132efd14dc215668bd7129a
BluSpring <bluspringindohq@gmail.com>
2026-08-01 14:29:03 +0800

    ensure sources is mutable

--- modules/resources/src/main/java/io/github/fabricators_of_create/porting_lib/resources/mixin/common/PackRepositoryMixin.java
+++ modules/resources/src/main/java/io/github/fabricators_of_create/porting_lib/resources/mixin/common/PackRepositoryMixin.java
@@ -1,24 +1,31 @@
 package io.github.fabricators_of_create.porting_lib.resources.mixin.common;
 
-import io.github.fabricators_of_create.porting_lib.resources.injections.PackRepositoryInjection;
-import net.minecraft.server.packs.repository.PackRepository;
-
-import net.minecraft.server.packs.repository.RepositorySource;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 
-import java.util.Set;
+import com.google.common.collect.ImmutableSet;
+
+import io.github.fabricators_of_create.porting_lib.resources.injections.PackRepositoryInjection;
+import net.minecraft.server.packs.repository.PackRepository;
+import net.minecraft.server.packs.repository.RepositorySource;
 
 @Mixin(PackRepository.class)
 public abstract class PackRepositoryMixin implements PackRepositoryInjection {
 	@Shadow
 	@Final
+	@Mutable
 	private Set<RepositorySource> sources;
 
 	@Override
 	public synchronized void port_lib$addPackFinder(RepositorySource packFinder) {
+		if (this.sources instanceof ImmutableSet<RepositorySource>)
+			this.sources = new LinkedHashSet<>(this.sources);
+
 		this.sources.add(packFinder);
 	}
 }
